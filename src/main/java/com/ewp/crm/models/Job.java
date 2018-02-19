d6eb1621a0965be1c6f627cc4ead2c58cf55a989
PahomovDm <pahomov_dm@mail.ru>
2018-02-19 22:20:48 +0300

    add model Job

---
+++ src/main/java/com/ewp/crm/models/Job.java
@@ -0,0 +1,75 @@
+package com.ewp.crm.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "job")
+public class Job {
+
+    @Id
+    @Column(name = "job_id")
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "organization")
+    private String organization;
+
+    @Column(name = "position")
+    private String position;
+
+    @OneToOne(mappedBy = "job")
+    private Client client;
+
+    public Job() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Job job = (Job) o;
+
+        if (!id.equals(job.id)) return false;
+        return client.equals(job.client);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + client.hashCode();
+        return result;
+    }
+}
