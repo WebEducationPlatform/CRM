991ff14952b025a49f04e4af5eb6fa4115158ce6
Nikita_Titov <nikitaunmortal@gmail.com>
2018-02-20 19:53:52 +0300

    All clients table and port

---
+++ src/main/resources/static/js/all-clients.js
@@ -0,0 +1,31 @@
+$(document).ready(
+    $("#searchInput").keyup(function () {
+        //split the current value of searchInput
+        var data = this.value.split(" ");
+        //create a jquery object of the rows
+        var jo = $("#table-body").find("tr");
+        if (this.value == "") {
+            jo.show();
+            return;
+        }
+        jo.hide();
+
+        jo.filter(function (i, v) {
+            var $t = $(this);
+            for (var d = 0; d < data.length; ++d) {
+                if ($t.is(":contains('" + data[d] + "')")) {
+                    return true;
+                }
+            }
+            return false;
+        }).show();
+    }).focus(function () {
+        this.value = "";
+        $(this).css({
+            "color": "black"
+        });
+        $(this).unbind('focus');
+    }).css({
+        "color": "#C0C0C0"
+    })
+);
