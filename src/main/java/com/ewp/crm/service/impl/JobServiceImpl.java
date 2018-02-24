07f9e3fc11c2735c6aa1d0725b2dd46deccd74bb
PahomovDm <pahomov_dm@mail.ru>
2018-02-25 02:01:53 +0300

    added job

---
+++ src/main/java/com/ewp/crm/service/impl/JobServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ewp.crm.service.impl;
+
+import com.ewp.crm.models.Job;
+import com.ewp.crm.repository.interfaces.JobDAO;
+import com.ewp.crm.service.interfaces.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private JobDAO jobDAO;
+
+    @Autowired
+    public JobServiceImpl(JobDAO jobDAO){
+        this.jobDAO = jobDAO;
+    };
+
+    @Override
+    public void add(Job job) {
+        jobDAO.saveAndFlush(job);
+    }
+
+    @Override
+    public void update(Job job) {
+        jobDAO.saveAndFlush(job);
+    }
+
+    @Override
+    public void delete(Job job) {
+        jobDAO.delete(job);
+    }
+
+    @Override
+    public List<Job> getJobsByClientId(Long id) {
+        return jobDAO.findAllByClientId(id);
+    }
+}
