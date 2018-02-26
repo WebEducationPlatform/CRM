cbcf7c754547af5a5ff1125deba1c50dd8bcee96
PahomovDm <pahomov_dm@mail.ru>
2018-02-26 21:42:39 +0300

    created model of Passport

---
+++ src/main/java/com/ewp/crm/models/Passport.java
@@ -0,0 +1,8 @@
+package com.ewp.crm.models;
+
+/**
+ * Created by Pahomov on 25.02.2018.
+ */
+public class Passport {
+
+}
---
+++ src/main/java/com/ewp/crm/service/interfaces/PassportService.java
@@ -0,0 +1,8 @@
+package com.ewp.crm.service.interfaces;
+
+/**
+ * Created by Pahomov on 26.02.2018.
+ */
+public class PassportService {
+
+}
