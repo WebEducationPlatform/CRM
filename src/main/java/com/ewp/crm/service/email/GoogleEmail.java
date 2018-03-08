50f6d3d3e80ee863fe9dfadf14df91188c0b8f9c
Nikita_Titov <nikitaunmortal@gmail.com>
2018-03-08 13:00:27 +0300

    Gmail integration

--- src/main/java/com/ewp/crm/service/email/GoogleEmail.java
+++ src/main/java/com/ewp/crm/service/email/GoogleEmail.java
@@ -1,9 +1,9 @@
 package com.ewp.crm.service.email;
 
 import com.ewp.crm.models.Client;
-import com.ewp.crm.models.Status;
 import com.ewp.crm.service.interfaces.ClientService;
 import com.ewp.crm.service.interfaces.StatusService;
+import com.ewp.crm.utils.converters.IncomeStringToClient;
 import org.apache.commons.mail.util.MimeMessageParser;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +14,7 @@ import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.mail.ImapIdleChannelAdapter;
 import org.springframework.integration.mail.ImapMailReceiver;
-import org.springframework.integration.mail.MailHeaders;
-import org.springframework.integration.mail.SearchTermStrategy;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 
@@ -30,131 +27,99 @@ import javax.mail.search.AndTerm;
 import javax.mail.search.FlagTerm;
 import javax.mail.search.FromTerm;
 import javax.mail.search.SearchTerm;
-import java.io.IOException;
 import java.util.Properties;
 
-//@Configuration
-//@EnableIntegration
+@Configuration
+@EnableIntegration
 public class GoogleEmail {
 
-    @Value("${google.mail.login}")
-    private String login;
+	private final BeanFactory beanFactory;
+	private final ClientService clientService;
+	private final StatusService statusService;
+	@Value("${google.mail.login}")
+	private String login;
+	@Value("${google.mail.password}")
+	private String password;
+	@Value("${mail.from}")
+	private String mailFrom;
+	@Value("${mail.imap.socketFactory.class}")
+	private String socketFactoryClass;
+	@Value("${mail.imap.socketFactory.fallback}")
+	private String socketFaktoryFallback;
+	@Value("${mail.store.protocol}")
+	private String protocol;
+	@Value("${mail.store.protocol}")
+	private String debug;
+	@Value("${mail.imap.server}")
+	private String imapServer;
 
-    @Value("${google.mail.password}")
-    private String password;
+	@Autowired
+	public GoogleEmail(BeanFactory beanFactory, ClientService clientService, StatusService statusService) {
+		this.beanFactory = beanFactory;
+		this.clientService = clientService;
+		this.statusService = statusService;
+	}
 
-    @Value("${mail.from}")
-    private String mailFrom;
+	private Properties javaMailProperties() {
+		Properties javaMailProperties = new Properties();
 
-    @Value("${mail.imap.socketFactory.class}")
-    private String socketFactoryClass;
+		javaMailProperties.setProperty("mail.imap.socketFactory.class", socketFactoryClass);
+		javaMailProperties.setProperty("mail.imap.socketFactory.fallback", socketFaktoryFallback);
+		javaMailProperties.setProperty("mail.store.protocol", protocol);
+		javaMailProperties.setProperty("mail.debug", debug);
 
-    @Value("${mail.imap.socketFactory.fallback}")
-    private String socketFaktoryFallback;
+		return javaMailProperties;
+	}
 
-    @Value("${mail.store.protocol}")
-    private String protocol;
+	@Bean
+	public ImapIdleChannelAdapter mailAdapter() {
+		ImapMailReceiver mailReceiver = new ImapMailReceiver("imaps://" + login + ":" + password + "@" + imapServer);
 
-    @Value("${mail.store.protocol}")
-    private String debug;
+		mailReceiver.setJavaMailProperties(javaMailProperties());
+		mailReceiver.setShouldDeleteMessages(false);
+		mailReceiver.setShouldMarkMessagesAsRead(true);
+		mailReceiver.setCancelIdleInterval(3600);
+		mailReceiver.setBeanFactory(beanFactory);
+		mailReceiver.setSearchTermStrategy(this::fromAndNotSeenTerm);
+		mailReceiver.afterPropertiesSet();
 
-    @Value("${mail.imap.server}")
-    private String imapServer;
+		ImapIdleChannelAdapter imapIdleChannelAdapter = new ImapIdleChannelAdapter(mailReceiver);
+		imapIdleChannelAdapter.setAutoStartup(true);
+		imapIdleChannelAdapter.setReconnectDelay(300000);
+		imapIdleChannelAdapter.setShouldReconnectAutomatically(true);
+		imapIdleChannelAdapter.setOutputChannel(directChannel());
+		imapIdleChannelAdapter.afterPropertiesSet();
 
-    private final BeanFactory beanFactory;
+		return imapIdleChannelAdapter;
+	}
 
-    private final ClientService clientService;
+	@Bean
+	public DirectChannel directChannel() {
+		DirectChannel directChannel = new DirectChannel();
+		directChannel.subscribe(new MessageHandler() {
+			public void handleMessage(Message<?> message) throws MessagingException {
+				MimeMessageParser parser = new MimeMessageParser((MimeMessage) message.getPayload());
+				try {
+					parser.parse();
+					Client client = IncomeStringToClient.convert(parser.getPlainContent() != null ? parser.getPlainContent() : parser.getHtmlContent());
+					if (client != null) {
+						clientService.addClient(client);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		return directChannel;
+	}
 
-    private final StatusService statusService;
+	private SearchTerm fromAndNotSeenTerm(Flags supportedFlags, Folder folder) {
+		try {
+			FromTerm fromTerm = new FromTerm(new InternetAddress(mailFrom));
+			return new AndTerm(fromTerm, new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+		} catch (AddressException e) {
+			throw new RuntimeException(e);
+		}
 
-    @Autowired
-    public GoogleEmail(BeanFactory beanFactory, ClientService clientService, StatusService statusService) {
-        this.beanFactory = beanFactory;
-        this.clientService = clientService;
-        this.statusService = statusService;
-    }
-
-    private Properties javaMailProperties() {
-        Properties javaMailProperties = new Properties();
-
-        javaMailProperties.setProperty("mail.imap.socketFactory.class", socketFactoryClass);
-        javaMailProperties.setProperty("mail.imap.socketFactory.fallback", socketFaktoryFallback);
-        javaMailProperties.setProperty("mail.store.protocol", protocol);
-        javaMailProperties.setProperty("mail.debug", debug);
-
-        return javaMailProperties;
-    }
-
-    @Bean
-    public ImapIdleChannelAdapter mailAdapter() {
-        ImapMailReceiver mailReceiver = new ImapMailReceiver("imaps://" + login + ":" + password + "@" + imapServer);
-
-        mailReceiver.setJavaMailProperties(javaMailProperties());
-        mailReceiver.setShouldDeleteMessages(false);
-        mailReceiver.setShouldMarkMessagesAsRead(true);
-        mailReceiver.setCancelIdleInterval(3600);
-        mailReceiver.setBeanFactory(beanFactory);
-        mailReceiver.setSearchTermStrategy(this::fromAndNotSeenTerm);
-        mailReceiver.afterPropertiesSet();
-
-        ImapIdleChannelAdapter imapIdleChannelAdapter = new ImapIdleChannelAdapter(mailReceiver);
-        imapIdleChannelAdapter.setAutoStartup(true);
-        imapIdleChannelAdapter.setReconnectDelay(300000);
-        imapIdleChannelAdapter.setShouldReconnectAutomatically(true);
-        imapIdleChannelAdapter.setOutputChannel(directChannel());
-        imapIdleChannelAdapter.afterPropertiesSet();
-
-        return imapIdleChannelAdapter;
-    }
-
-    @Bean
-    public DirectChannel directChannel() {
-        DirectChannel directChannel = new DirectChannel();
-        directChannel.subscribe(new MessageHandler() {
-            public void handleMessage(Message<?> message) throws MessagingException {
-                MimeMessageParser parser = new MimeMessageParser((MimeMessage) message.getPayload());
-                try {
-                    parser.parse();
-                    clientService.addClient(parseMail(parser.getPlainContent()));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        return directChannel;
-    }
-
-    private SearchTerm fromAndNotSeenTerm(Flags supportedFlags, Folder folder) {
-        try {
-            FromTerm fromTerm = new FromTerm(new InternetAddress(mailFrom));
-            return new AndTerm(fromTerm, new FlagTerm(new Flags(Flags.Flag.SEEN), false));
-        } catch (AddressException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private Client parseMail(String mail) {
-        Client client = new Client();
-        String[] stringsOfMail = mail.split("\n");
-        for (String str : stringsOfMail) {
-            if (str.contains("Имя: ")) {
-                client.setName(str.replace("Имя: ", ""));
-            }
-            if (str.contains("Name 3: ")) {
-                client.setName(str.replace("Name 3: ", ""));
-            }
-            if (str.contains("Email 2:")) {
-                client.setEmail(str.substring(10));
-            }
-            if (str.contains("Телефон: ")) {
-                client.setPhoneNumber(str.replace("Телефон: ", ""));
-            }
-            if (str.contains("phone: ")) {
-                client.setPhoneNumber(str.replace("phone: ", ""));
-            }
-        }
-        client.setStatus(statusService.get(1L));
-        return client;
-    }
+	}
 }
---
+++ src/main/java/com/ewp/crm/utils/converters/IncomeStringToClient.java
@@ -0,0 +1,125 @@
+package com.ewp.crm.utils.converters;
+
+import com.ewp.crm.models.Client;
+import com.ewp.crm.models.SocialNetwork;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class IncomeStringToClient {
+
+	private static Logger logger = LoggerFactory.getLogger(IncomeStringToClient.class);
+
+	public static Client convert(String income) {
+		Client client = null;
+		if (income != null && !income.isEmpty()) {
+			String[] workArr = getArrayFromIncomeData(income);
+			if (income.contains("Name 3:")) {
+				client = firstPlainTextTemplate(workArr);
+			} else if (income.contains("Name:")) {
+				client = secondPlainTextTemplate(workArr);
+			} else if (income.contains("Имя:")) {
+				client = thirdPlainTextTemplate(workArr);
+			} else {
+				logger.error("The incoming email does not match any of the templates!!!");
+			}
+		}
+
+		return client;
+	}
+
+	private static String[] getArrayFromIncomeData(String income) {
+		if (income.contains("<br>")) {
+			String[] temp = income.replaceAll("<br><br>", "<br>").split("<br>");
+			System.arraycopy(temp, 1, temp, 0, temp.length - 1);
+			return temp;
+		} else {
+			return income.split("\r\n");
+		}
+	}
+
+	private static Client firstPlainTextTemplate(String[] workArr) {
+		Client resultClient = new Client();
+		String[] parsedName = parseName(getValueOfField(workArr[0]));
+		if (parsedName != null) {
+			resultClient.setName(parsedName[0]);
+			resultClient.setLastName(parsedName[1]);
+		} else {
+			resultClient.setName(getValueOfField(workArr[0]));
+		}
+		resultClient.setSocialNetworks(Collections.singletonList(getSocialNetwork(getValueOfField(workArr[1]))));
+		resultClient.setPhoneNumber(getValueOfField(workArr[2]));
+		resultClient.setEmail(getValueOfField(workArr[3]));
+		resultClient.setComment(getValueOfField(workArr[4]));
+		return resultClient;
+	}
+
+	private static Client secondPlainTextTemplate(String[] workArr) {
+		Client resultClient = new Client();
+		String[] parsedName = parseName(getValueOfField(workArr[0]));
+		if (parsedName != null) {
+			resultClient.setName(parsedName[0]);
+			resultClient.setLastName(parsedName[1]);
+		} else {
+			resultClient.setName(getValueOfField(workArr[0]));
+		}
+		resultClient.setSocialNetworks(Collections.singletonList(getSocialNetwork(getValueOfField(workArr[1]))));
+		resultClient.setEmail(getValueOfField(workArr[2]));
+		resultClient.setPhoneNumber(getValueOfField(workArr[3]));
+		resultClient.setCity(getValueOfField(workArr[4]));
+		resultClient.setComment(getValueOfField(workArr[5]));
+		return resultClient;
+	}
+
+	private static Client thirdPlainTextTemplate(String[] workArr) {
+		Client resultClient = new Client();
+		String[] parsedName = parseName(getValueOfField(workArr[6]));
+		if (parsedName != null) {
+			resultClient.setName(parsedName[0]);
+			resultClient.setLastName(parsedName[1]);
+		} else {
+			resultClient.setName(getValueOfField(workArr[6]));
+		}
+		resultClient.setSocialNetworks(Collections.singletonList(getSocialNetwork(getValueOfField(workArr[7]))));
+		resultClient.setPhoneNumber(getValueOfField(workArr[8]));
+		resultClient.setCity(getValueOfField(workArr[9]));
+		resultClient.setEmail(getValueOfField(workArr[10]));
+		return resultClient;
+	}
+
+	private static String[] parseName(String fullName) {
+		if (StringUtils.countOccurrencesOf(fullName, " ") == 1) {
+			return fullName.split(" ", 2);
+		}
+		return null;
+	}
+
+	private static SocialNetwork getSocialNetwork(String link) {
+		if (link.startsWith("https://")) {
+			link = link.substring(8);
+		}
+		SocialNetwork socialNetwork = new SocialNetwork();
+		if (link.startsWith("vk.com") || link.startsWith("m.vk.com")) {
+			socialNetwork.setLink(link);
+			socialNetwork.setSocialMarker(SocialNetwork.SocialMarker.VK);
+		} else if (link.startsWith("www.facebook.com") || link.startsWith("m.facebook.com")) {
+			socialNetwork.setLink(link);
+			socialNetwork.setSocialMarker(SocialNetwork.SocialMarker.FACEBOOK);
+		}
+		return socialNetwork;
+	}
+
+	private static String getValueOfField(String fullString) {
+		String result;
+		try {
+			result = fullString.substring(fullString.indexOf(":") + 2);
+		} catch (IndexOutOfBoundsException e) {
+			result = "";
+		}
+		return result;
+	}
+
+}
