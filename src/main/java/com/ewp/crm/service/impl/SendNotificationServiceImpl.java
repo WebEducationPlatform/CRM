6d2485e3184cb0efa86afc70da6d79278c18838d
Fedor <extr1811@gmail.com>
2018-04-19 22:42:31 +0700

    remove forEach and build regex pattern

--- src/main/java/com/ewp/crm/service/impl/SendNotificationServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/SendNotificationServiceImpl.java
@@ -9,6 +9,8 @@ import com.ewp.crm.service.interfaces.SendNotificationService;
 import com.ewp.crm.service.interfaces.UserService;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,16 +33,19 @@ public class SendNotificationServiceImpl implements SendNotificationService {
 
 	@Override
 	public void sendNotification(String content, Client client) {
-		List<User> users = userService.getAll();
-		for (User user : users) {
-			if (content.contains('@' + user.getFullCombinedName())) {
-				User userToNotify = userService.getUserByFirstNameAndLastName(user.getFirstName(), user.getLastName());
-				if (user.isEnableNotifications()) {
-					mailSendService.sendNotificationMessage(userToNotify);
-				}
-				Notification notification = new Notification(client, userToNotify);
-				notificationService.addNotification(notification);
+		String regexForContent = "\\B\\@\\w+";
+		String regexForSplit = "(?=[A-Z])";
+		Pattern pattern = Pattern.compile(regexForContent);
+		Matcher matcher = pattern.matcher(content);
+
+		while (matcher.find()) {
+			String[] result = matcher.group().split(regexForSplit);
+			User userToNotify = userService.getUserByFirstNameAndLastName(result[1], result[2]);
+			if (userToNotify.isEnableNotifications()) {
+				mailSendService.sendNotificationMessage(userToNotify);
 			}
+			Notification notification = new Notification(client, userToNotify);
+			notificationService.addNotification(notification);
 		}
 	}
 }
