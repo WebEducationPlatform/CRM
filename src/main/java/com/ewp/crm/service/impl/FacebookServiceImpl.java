9a132e88c4b9087c2b91091bfb0e1d1d504e89a7
PlatonJKE <platon69@bk.ru>
2018-07-01 22:54:22 +0300

    ge мessages from community page

--- src/main/java/com/ewp/crm/service/impl/FacebookServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/FacebookServiceImpl.java
@@ -3,22 +3,18 @@ package com.ewp.crm.service.impl;
 import com.ewp.crm.configs.inteface.FacebookConfig;
 import com.ewp.crm.exceptions.util.FBAccessTokenException;
 import com.ewp.crm.service.interfaces.FacebookService;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,12 +27,14 @@ public class FacebookServiceImpl implements FacebookService {
 	private String version;
 	private String pageToken;
 	private String pageId;
+	private final RestTemplate restTemplate;
 
 	@Autowired
-	public FacebookServiceImpl(FacebookConfig facebookConfig) {
+	public FacebookServiceImpl(FacebookConfig facebookConfig, RestTemplate restTemplate) {
 		this.version = facebookConfig.getVersion();
 		this.pageToken = facebookConfig.getPageToken();
 		this.pageId = facebookConfig.getPageId();
+		this.restTemplate = restTemplate;
 	}
 
 	private final String FB_API_METHOD_TEMPLATE = "https://graph.facebook.com/";
@@ -45,24 +43,17 @@ public class FacebookServiceImpl implements FacebookService {
 		if (pageToken == null) {
 			throw new FBAccessTokenException("Facebook access token has not got");
 		}
-		String uriGetMassages = FB_API_METHOD_TEMPLATE  + version + "/" + pageId + "/" +
+		URI uri = URI.create(FB_API_METHOD_TEMPLATE  + version + "/" + pageId + "/" +
 				"?fields=about,conversations%7Bmessages%7Bfrom,message%7D%7D" +
-				"&access_token=" + pageToken;
+				"&access_token=" + pageToken);
 		try {
-			HttpGet httpGetMessages = new HttpGet(uriGetMassages);
-			HttpClient httpClient = HttpClients.custom()
-					.setDefaultRequestConfig(RequestConfig.custom()
-							.setCookieSpec(CookieSpecs.STANDARD).build())
-					.build();
-			HttpResponse response = httpClient.execute(httpGetMessages);
-			String result = EntityUtils.toString(response.getEntity());
-			JSONObject json = new JSONObject(result);
+			ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, null, String.class);
+			JSONObject json = new JSONObject(result.getBody());
 			JSONObject conversations = (JSONObject) json.get("conversations");
 			JSONArray jsonData = conversations.getJSONArray("data");
 			List<String> resultList = new ArrayList<>();
 			int count = 0;
-			while (jsonData.length() != 0) {
-
+			while (count != jsonData.length()) {
 				JSONObject jsonDataObj = jsonData.getJSONObject(count);
 				JSONObject jsonDataObjMessages = jsonDataObj.getJSONObject("messages");
 				JSONArray nestedDatajsonjMessages = jsonDataObjMessages.getJSONArray("data");
@@ -71,15 +62,13 @@ public class FacebookServiceImpl implements FacebookService {
 					JSONObject jsonMessage = nestedDatajsonjMessages.getJSONObject(i);
 					String name = jsonMessage.getJSONObject("from").getString("name");
 					String message = jsonMessage.getString("message");
-					System.out.println(name + ": " + message);
-					resultList.add(jsonMessage.getString("message"));
+					String conversation = name + ": " + message;
+					resultList.add(conversation);
 				}
 			}
 				return Optional.of(resultList);
 		} catch (JSONException e) {
 			logger.error("Can not read message from JSON ", e);
-		} catch (IOException e) {
-			logger.error("Failed to connect to Facebook server ", e);
 		}
 		return Optional.empty();
 	}
