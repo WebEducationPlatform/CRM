219d188b85a1560ab5143995fc39995049a31d75
mcdncrnc73 <mcdncrnc73@gmail.com>
2018-07-12 16:37:23 +0300

    parsing form

--- src/main/java/com/ewp/crm/utils/converters/IncomeStringToClient.java
+++ src/main/java/com/ewp/crm/utils/converters/IncomeStringToClient.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class IncomeStringToClient {
@@ -23,96 +26,119 @@ public class IncomeStringToClient {
         this.socialNetworkTypeService = socialNetworkTypeService;
     }
 
-	public Client convert(String income) {
-		Client client = null;
-		if (income != null && !income.isEmpty()) {
-			String workString = prepareForm(income);
-			if (income.contains("Форма: Начать обучение")) {
-				client = parseClientFormOne(workString);
-			} else if (income.contains("Форма: Месяц в подарок")) {
-				client = parseClientFormOne(workString);
-			} else if (income.contains("Форма: Остались вопросы")) {
-				client = parseClientFormTwo(workString);
-			} else if (income.contains("Форма: Java Test")) {
-				client = parseClientFormFour(workString);
-			} else {
-				logger.error("The incoming email does not match any of the templates!!!");
-			}
-		}
-		return client;
-	}
+    public Client convert(String income) {
+        Client client = null;
+        if (income != null && !income.isEmpty()) {
+            String workString = prepareForm(income);
+            if (income.contains("Начать обучение")) {
+                client = parseClientFormOne(workString);
+            } else if (income.contains("Месяц в подарок")) {
+                client = parseClientFormOne(workString);
+            } else if (income.contains("Остались вопросы")) {
+                client = parseClientFormTwo(workString);
+            } else if (income.contains("Java Test")) {
+                client = parseClientFormFour(workString);
+            } else {
+                logger.error("The incoming email does not match any of the templates!!!");
+            }
+        }
+        return client;
+    }
 
-	private static String prepareForm(String text) {
-		if (text.contains("<br>")) {
-			text = text.replaceAll("<br>", "");
-		}
-		if (text.contains("Имя: ")) {
-			return text.substring(text.indexOf("Имя: "));
-		}
-		return text.substring(text.indexOf("Name "), text.length()).replaceAll("(\r\n|\n)", "");
-	}
+    private static String prepareForm(String text) {
+        return text.substring(text.indexOf("Форма:"), text.length())
+                .replaceAll("<b>|</b>|(\\r\\n|\\n)", "");
+    }
 
-	private Client parseClientFormOne(String form) {
-		Client client = new Client();
-		String[] result = form.split(":\\s");
-		setClientName(client, result[1].substring(0, result[1].indexOf(" Соц ")));
-		client.setSocialNetworks(Collections.singletonList(getSocialNetwork(result[2].substring(0, result[2].indexOf(" Телефон")))));
-		client.setPhoneNumber(result[3].substring(0, result[3].indexOf(" Страна")));
-		client.setCountry(result[4].substring(0, result[4].indexOf(" Город")));
-		client.setCity(result[5].substring(0, result[5].indexOf(" Email")));
-		if (form.contains("Согласен")) {
-			client.setEmail(result[6].substring(0, result[6].indexOf(" Согласен")));
-		} else {
-			client.setEmail(result[6]);
-			client.setClientDescriptionComment("На пробные 3 дня");
-		}
-		return client;
-	}
+    private Client parseClientFormOne(String form) {
+        Client client = new Client();
+        String s2 = form.substring(form.indexOf("Name"), form.length())
+                .replaceAll(" ", "")
+                .replaceAll("Name[0-9]", "Name")
+                .replaceAll("Email[0-9]", "Email");
+        String[] res = s2.split("<br/>");
+        Map<String, String> map = ClientData(res);
+        setClientName(client, map.get("Name"));
+        client.setPhoneNumber(map.get("Телефон"));
+        client.setCountry(map.get("Страна"));
+        client.setCity(map.get("Город"));
+        if (map.containsKey("Соцсеть")) {
+            client.setSocialNetworks(Collections.singletonList(getSocialNetwork(map.get("Соцсеть"))));
+        }
+        if (form.contains("Согласен")) {
+            client.setEmail(map.get("Email"));
+        } else {
+            client.setEmail(map.get("Email"));
+            client.setClientDescriptionComment("На пробные 3 дня");
+        }
+        return client;
+    }
 
-	private Client parseClientFormTwo(String form) {
-		Client client = new Client();
-		String[] result = form.split(":\\s");
-		setClientName(client, result[1].substring(0, result[1].indexOf(" Social")));
-		client.setSocialNetworks(Collections.singletonList(getSocialNetwork(result[2].substring(0, result[2].indexOf(" Phone")))));
-		client.setPhoneNumber(result[3].substring(0, result[3].indexOf(" Email")));
-		client.setEmail(result[4].substring(0, result[4].indexOf(" Vopros")));
-		client.setClientDescriptionComment(result[5]);
-		return client;
-	}
+    private Client parseClientFormTwo(String form) {
+        Client client = new Client();
+        String s2 = form.substring(form.indexOf("Name"), form.length())
+                .replaceAll(" ", "")
+                .replaceAll("Name[0-9]", "Name")
+                .replaceAll("Email[0-9]", "Email");
+        String[] res = s2.split("<br/>");
+        Map<String, String> map = ClientData(res);
+        setClientName(client, map.get("Name"));
+        client.setPhoneNumber(map.get("Phone"));
+        client.setClientDescriptionComment(map.get("Vopros"));
+        if (map.containsKey("Social")) {
+            client.setSocialNetworks(Collections.singletonList(getSocialNetwork(map.get("Social"))));
+        }
+        return client;
+    }
 
-	private Client parseClientFormFour(String form) {
-		Client client = new Client();
-		String[] result = form.split(":\\s");
-		setClientName(client, result[1].substring(0, result[1].indexOf(" Social")));
-		client.setSocialNetworks(Collections.singletonList(getSocialNetwork(result[2].substring(0, result[2].indexOf(" Phone")))));
-		client.setPhoneNumber(result[3].substring(0, result[3].indexOf(" City")));
-		client.setCountry(result[4].substring(0, result[4].indexOf(" Email")));
-		client.setEmail(result[5].substring(0, result[5].indexOf("\r")));
-		client.setClientDescriptionComment("Проходил Тест");
-		return client;
-	}
+    private Client parseClientFormFour(String form) {
+        Client client = new Client();
+        String s = form.replaceAll(" ", "");
+        String s2 = s.substring(s.indexOf("Имя"), s.length());
+        String[] res = s2.split("<br/>");
+        Map<String, String> map = ClientData(res);
+        setClientName(client, map.get("Имя"));
+        if (map.containsKey("Social2")) {
+            client.setSocialNetworks(Collections.singletonList(getSocialNetwork(map.get("Social2"))));
+        }
+        client.setPhoneNumber(map.get("Phone6"));
+        client.setCountry(map.get("City6"));
+        client.setEmail(map.get("Email2"));
+        client.setClientDescriptionComment("Проходил Тест");
+        return client;
+    }
 
-	private SocialNetwork getSocialNetwork(String link) {
-		SocialNetwork socialNetwork = new SocialNetwork();
-		if (link.contains("vk.com") || link.contains("m.vk.com")) {
-			socialNetwork.setLink(link);
-			socialNetwork.setSocialNetworkType(socialNetworkTypeService.getByTypeName("vk"));
-		} else if (link.startsWith("www.facebook.com") || link.startsWith("m.facebook.com")) {
-			socialNetwork.setLink(link);
-			socialNetwork.setSocialNetworkType(socialNetworkTypeService.getByTypeName("facebook"));
-		} else {
-			socialNetwork = null;
-		}
-		return socialNetwork;
-	}
+    private SocialNetwork getSocialNetwork(String link) {
+        SocialNetwork socialNetwork = new SocialNetwork();
+        if (link.contains("vk.com") || link.contains("m.vk.com")) {
+            socialNetwork.setLink(link);
+            socialNetwork.setSocialNetworkType(socialNetworkTypeService.getByTypeName("vk"));
+        } else if (link.startsWith("www.facebook.com") || link.startsWith("m.facebook.com")) {
+            socialNetwork.setLink(link);
+            socialNetwork.setSocialNetworkType(socialNetworkTypeService.getByTypeName("facebook"));
+        } else {
+            socialNetwork = null;
+        }
+        return socialNetwork;
+    }
 
-	private void setClientName(Client client, String fullName) {
-		if (StringUtils.countOccurrencesOf(fullName, " ") == 1) {
-			String[] full = fullName.split(" ");
-			client.setName(full[0]);
-			client.setLastName(full[1]);
-		} else {
-			client.setName(fullName);
-		}
-	}
+    private Map<String, String> ClientData(String[] res) {
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < res.length; i++) {
+            String name = res[i].substring(0, res[i].indexOf(":"));
+            String value = res[i].substring(res[i].indexOf(":") + 1, res[i].length());
+            map.put(name, value);
+        }
+        return map;
+    }
+
+    private void setClientName(Client client, String fullName) {
+        if (StringUtils.countOccurrencesOf(fullName, " ") == 1) {
+            String[] full = fullName.split(" ");
+            client.setName(full[0]);
+            client.setLastName(full[1]);
+        } else {
+            client.setName(fullName);
+        }
+    }
 }
--- src/main/resources/static/js/all-clients.js
+++ src/main/resources/static/js/all-clients.js
@@ -159,7 +159,7 @@ $(document).ready(function () {
 
 
     win.scroll(function () {
-        if ($(document).height() - win.height() < win.scrollTop() + 10) {
+        if ($(document).height() - win.height() === win.scrollTop()) {
 
             $.get('/rest/client/pagination/get', {page : page}, function upload(clients) {
                 let table = $("#clients-table").find("tbody");
