c39c0eec0e169d72b6fc0bf075fa77afbb8a3b59
StalnoyKapibar <33718317+StalnoyKapibar@users.noreply.github.com>
2018-07-21 17:21:05 +0300

    Final Version

--- src/main/java/com/ewp/crm/repository/interfaces/CallRecordRepository.java
+++ src/main/java/com/ewp/crm/repository/interfaces/CallRecordRepository.java
@@ -4,6 +4,4 @@ import com.ewp.crm.models.CallRecord;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CallRecordRepository extends CommonGenericRepository<CallRecord> {
-    @Query("SELECT cl FROM CallRecord cl WHERE cl.id = (SELECT max(c.id) from CallRecord c)")
-    CallRecord findCallRecordMaxId();
 }
