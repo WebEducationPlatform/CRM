4535cf0fd94f077d4d703165e2e0d0612d5d186e
Graur Andrew <ancongr@yandex.ru>
2018-07-21 15:48:25 +0500

    fix review remarks

--- src/main/java/com/ewp/crm/service/impl/YoutubeServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/YoutubeServiceImpl.java
@@ -39,7 +39,7 @@ public class YoutubeServiceImpl implements YoutubeService {
                 logger.info("Live stream is in action");
             }
         } catch (IndexOutOfBoundsException e) {
-            logger.error("Live events in Youtube channel isn't existing");
-        }
+            logger.error("Live events in Youtube channel don't exist");
+            }
     }
 }
