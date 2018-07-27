39a266e5ac86648a0692848c5a14c7694b02817a
Tim <mddtimur@gmail.com>
2018-07-27 11:01:58 +0300

    Added Apache diff library, made Client class implement Diffable

--- src/main/java/com/ewp/crm/service/impl/ClientHistoryServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/ClientHistoryServiceImpl.java
@@ -7,6 +7,7 @@ import com.ewp.crm.models.User;
 import com.ewp.crm.repository.interfaces.ClientHistoryRepository;
 import com.ewp.crm.service.interfaces.ClientHistoryService;
 import com.ewp.crm.service.interfaces.MessageService;
+import org.apache.commons.lang3.builder.DiffResult;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,9 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -138,103 +136,28 @@ public class ClientHistoryServiceImpl implements ClientHistoryService {
 	@Override
 	public ClientHistory createHistory(User admin, Client prev, Client current, ClientHistory.Type type) {
 		ClientHistory clientHistory = new ClientHistory(type);
-		//This kostyl use because view represent null properties as empty string "".
-		if (current.getEmail().isEmpty()) {
-			current.setEmail(null);
-		}
-		if (current.getPhoneNumber().isEmpty()) {
-			current.setPhoneNumber(null);
-		}
-		clientHistory.setTitle(admin.getFullName() + " " + type.getInfo());
-		// if user updated client, which have no changes.
-		if (current.equals(prev)) {
-			return clientHistory;
-		}
-		String buildChanges = null;
-		try {
-			buildChanges = findChanges(prev, current);
-		} catch (IllegalAccessException e) {
-			logger.info("Can't find changes", e);
-		}
-		Message message = messageService.addMessage(Message.Type.DATA, buildChanges);
+
+        // if user updated client, which has no changes.
+        if (current.equals(prev)) {
+            logger.info("Can't find changes");
+            return clientHistory;
+        }
+
+        clientHistory.setTitle(admin.getFullName() + " " + type.getInfo());
+        //TODO почему массивы с социальными сетями указывают на разные массивы???
+		DiffResult diffs = prev.diff(current);
+
+        StringBuilder content = new StringBuilder();
+
+		diffs.getDiffs().stream().map(
+				d -> d.getFieldName() + ": " + d.getLeft() + " -> " + d.getRight())
+				.forEach(str -> content.append(str).append("<br/>"));
+
+		Message message = messageService.addMessage(Message.Type.DATA, content.toString());
+
 		clientHistory.setMessage(message);
+
 		clientHistory.setLink("/client/message/info/" + message.getId());
 		return clientHistory;
-	}
-
-	private String findChanges(Client prev, Client current) throws IllegalAccessException {
-		StringBuilder changesBuild = new StringBuilder();
-		Field[] prevFields = prev.getClass().getDeclaredFields();
-		Field[] currentFields = current.getClass().getDeclaredFields();
-
-		for (int i = 0; i < prevFields.length; i++) {
-			Field prevField = prevFields[i];
-			Field currentField = currentFields[i];
-			prevField.setAccessible(true);
-			currentField.setAccessible(true);
-			Object data1 = prevField.get(prev);
-			Object data2 = prevField.get(current);
-			if (data1 != null && data2 != null) {
-				if (data1 instanceof Collection) {
-					if (!data1.equals(data2)) {
-						iterateCollection(data1, data2, changesBuild);
-					}
-				} else if (!data1.equals(data2)) {
-					changesBuild.append(getRow(data1, data2));
-				}
-			}
-		}
-		return transformChangesToHtml(changesBuild.toString());
-	}
-
-	/*
-		iterate collection and build changes
-	 */
-	private void iterateCollection(Object data1, Object data2, StringBuilder changesBuild) {
-		Collection<?> collection1 = ((Collection<?>) data1);
-		Collection<?> collection2 = ((Collection<?>) data2);
-		Iterator<?> iterator1 = collection1.iterator();
-		Iterator<?> iterator2 = collection2.iterator();
-		while (iterator1.hasNext() && iterator2.hasNext()) {
-			Object element1 = iterator1.next();
-			Object element2 = iterator2.next();
-			if(!element1.equals(element2)) {
-				changesBuild.append(getRow(element1,element2));
-			}
-		}
-		while (iterator1.hasNext()) {
-			changesBuild.append("{prev}");
-			changesBuild.append(iterator1.next());
-			changesBuild.append("{close}");
-			changesBuild.append(" удалено");
-			changesBuild.append("{br}");
-		}
-		while (iterator2.hasNext()) {
-			changesBuild.append("{current}");
-			changesBuild.append(iterator2.next());
-			changesBuild.append("{close}");
-			changesBuild.append(" добавлено");
-			changesBuild.append("{br}");
-		}
-	}
-
-	private String getRow(Object obj1, Object obj2) {
-		return "{prev}" + obj1 + "{close} изменено на {current}" + obj2 + "{close}{br}";
-	}
-
-	//transform changes to html, use in CK editor
-	private String transformChangesToHtml(String changes) {
-		return changes.replaceAll("\\{prev}", "<span style=\"background-color:#DCDCDC\">")
-				.replaceAll("\\{current}","<span style=\"background-color:#90EE90\">")
-				.replaceAll("\\{close}","</span>")
-				.replaceAll("\\{br}","<br>");
-	}
-
-	//transform to normal String
-	private String transformChanges(String changes) {
-		return changes.replaceAll("\\{prev}", "")
-				.replaceAll("\\{current}","")
-				.replaceAll("\\{close}","")
-				.replaceAll("\\{br}","\n");
 	}
 }
