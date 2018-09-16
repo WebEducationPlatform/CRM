5b726a20d109544df777f21c9b44cff29501964c
shonorov <shonorov@gmail.com>
2018-09-16 18:46:39 +0300

    student statuses

---
+++ src/main/resources/static/js/all-students-status.js
@@ -0,0 +1,12 @@
+$('.button_edit_status').click(function () {
+    // let currentModal = $('#student-edit-modal');
+    console.log("edit " + this.value);
+    // currentModal.data('student_id', this.value);
+    // currentModal.modal('show');
+});
+
+$('.button_delete_status').click(function () {
+    console.log("delete " + this.value);
+    // currentModal.data('student_id', this.value);
+    // currentModal.modal('show');
+});
