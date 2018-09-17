ec111ef94eeb2614a46297759e89a5ba10e7d4e7
shonorov <shonorov@gmail.com>
2018-09-17 15:28:48 +0300

    student statuses

--- src/main/java/com/ewp/crm/controllers/rest/StudentStatusRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/StudentStatusRestController.java
@@ -3,11 +3,10 @@ package com.ewp.crm.controllers.rest;
 import com.ewp.crm.models.StudentStatus;
 import com.ewp.crm.service.interfaces.StudentStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +25,13 @@ public class StudentStatusRestController {
     @GetMapping
     public ResponseEntity<List<StudentStatus>> getAllStudentStatuses() {
         return ResponseEntity.ok(studentStatusService.getAll());
+    }
+
+    @PostMapping ("/create")
+    private HttpStatus createStudentStatus(@RequestBody StudentStatus studentStatus) {
+        System.out.println("222 " + studentStatus);
+        //TODO Null pointer
+        studentStatusService.add(studentStatus);
+        return HttpStatus.OK;
     }
 }
