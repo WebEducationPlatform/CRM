aa814bcac2f254fdb018565a0e890d150eb57cc7
shonorov <shonorov@gmail.com>
2018-09-17 15:28:48 +0300

    student statuses

--- src/main/resources/static/js/all-students-status.js
+++ src/main/resources/static/js/all-students-status.js
@@ -10,3 +10,33 @@ $('.button_delete_status').click(function () {
     // currentModal.data('student_id', this.value);
     // currentModal.modal('show');
 });
+
+$('#create_status').click(function () {
+    let currentModal = $('#student-status-create-modal');
+    // currentModal.data('student_id', this.value);
+    currentModal.modal('show');
+});
+
+$('#create_student_status').click(function () {
+    let status = $("#status-name").val();
+    let data = {status : status};
+    console.log(data);
+    $.ajax({
+        type: 'POST',
+        url: '/rest/student/status/create',
+        contentType : "application/json; charset=UTF-8",
+        encoding: "UTF-8",
+        dataType: "JSON",
+        data: JSON.stringify(data),
+        success: function () {
+            // location.reload();
+        }
+    })
+
+});
+
+// $(function () {
+//     $('#student-status-create-modal').on('show.bs.modal', function () {
+//
+//     });
+// });
