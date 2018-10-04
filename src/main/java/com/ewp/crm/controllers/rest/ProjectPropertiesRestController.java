29c518d6b71a9c217355a62c2bbd865b117de905
shonorov <shonorov@gmail.com>
2018-10-04 17:16:57 +0300

    Finished back and front, TODO: scheduler

--- src/main/java/com/ewp/crm/controllers/rest/ProjectPropertiesRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/ProjectPropertiesRestController.java
@@ -1,14 +1,15 @@
 package com.ewp.crm.controllers.rest;
 
 import com.ewp.crm.models.ProjectProperties;
+import com.ewp.crm.service.interfaces.MessageTemplateService;
 import com.ewp.crm.service.interfaces.ProjectPropertiesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalTime;
 
 @RestController
 @RequestMapping("/rest/properties")
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectPropertiesRestController {
 
     private final ProjectPropertiesService projectPropertiesService;
+    private final MessageTemplateService messageTemplateService;
 
     @Autowired
-    public ProjectPropertiesRestController(ProjectPropertiesService projectPropertiesService) {
+    public ProjectPropertiesRestController(ProjectPropertiesService projectPropertiesService, MessageTemplateService messageTemplateService) {
         this.projectPropertiesService = projectPropertiesService;
+        this.messageTemplateService = messageTemplateService;
     }
 
     @GetMapping
@@ -29,6 +32,22 @@ public class ProjectPropertiesRestController {
             projectProperties = new ProjectProperties();
         }
         return new ResponseEntity<>(projectProperties, HttpStatus.OK);
+    }
+
+    @PostMapping("/email-notification")
+    public HttpStatus setPaymentNotificationSettings(@RequestParam( name = "paymentMessageTemplate") Long templateId,
+                                                     @RequestParam( name = "paymentNotificationTime") String time,
+                                                     @RequestParam( name = "paymentNotificationEnabled") Boolean enabled) {
+        ProjectProperties current = projectPropertiesService.getOrCreate();
+        if (templateId != null) {
+            current.setPaymentMessageTemplate(messageTemplateService.get(templateId));
+        } else {
+            current.setPaymentMessageTemplate(null);
+        }
+        current.setPaymentNotificationTime(LocalTime.parse(time));
+        current.setPaymentNotificationEnabled(enabled);
+        projectPropertiesService.update(current);
+        return HttpStatus.OK;
     }
 
     @GetMapping("/status")
