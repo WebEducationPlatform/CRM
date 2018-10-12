2feaec9255d29132981573d28a81b3c8198f3536
u <jek.kalashnikov@gmail.com>
2018-10-12 14:28:31 +0300

    d

--- src/main/java/com/ewp/crm/service/impl/SendNotificationServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/SendNotificationServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
