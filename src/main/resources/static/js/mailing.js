c00d6da9935e022c666c1bd11f5d5f909ec99de0
Tyurin Denis <dent.ttt@gmail.com>
2018-11-14 22:46:45 +0400

    mailing new ckeditor 14-11-18, 22:45

--- src/main/resources/static/js/mailing.js
+++ src/main/resources/static/js/mailing.js
@@ -1,54 +1,26 @@
-let type = 'email';
-let text;
+const BUTTON_INFO_CLASS = "btn-info";
+const BUTTON_SECONDARY_CLASS = "btn-secondary";
+const DROP_ZONE_IS_DRAGOVER_CLASS = "drop-zone-is-dragover";
+const BADGE_SUCCESS_CLASS = "badge-success";
+const BADGE_WARNING_CLASS = "badge-warning";
+const EDITOR = "editor";
+const URL_POST_DATA = "/client/mailing/send";
+const SEND_EMAILS = "Enter the recipients email address here:";
+const SEND_SMSS = "Enter phone numbers here:";
+const SEND_TO_VK = "Enter VK ids here:";
 
-function switchMailingType() {
-    var selected = $('#socNetworkChoose').val();
-    if (selected === 'email') {
-        type = 'email';
-        $('#field').show();
-        $('#vkArea').hide();
-        $('#smsArea').hide();
-    } else if (selected === 'vk') {
-        type = 'vk';
-        $('#field').hide();
-        $('#vkArea').show();
-        $('#smsArea').hide();
-        text = document.getElementById("vkArea1");
-    }
-    else if (selected === 'sms') {
-        type = 'sms';
-        $('#field').hide();
-        $('#vkArea').hide();
-        $('#smsArea').show();
-        text = document.getElementById("smsArea1");
-    }
-}
+var messageType = 'email';
 
-$(document).ready(function () {
-    editor = CKEDITOR.replace('body1', {
-        allowedContent: true,
-        height: '400px',
-    });
-    editor.addCommand("infoCommend", {
-        exec: function (edt) {
-            $("#infoModal").modal('show');
-        }
-    });
-    editor.ui.addButton('SuperButton', {
-        label: "Info",
-        command: 'infoCommend',
-        toolbar: 'styles',
-        icon: 'info.png'
-    });
-});
-
-
-function mail(sendnow) {
-    let date = $('#mailingDate').val();
-    let templateText = CKEDITOR.instances['body1'].getData();
-    let clientData = $('#clientData1').val();
+function sendMessages(sendnow) {
+    console.warn(sendnow);
+    let date = $('#messageSendingTime').val();
+    console.warn(date);
+    let text = CKEDITOR.instances.editor.getData();
+    console.warn(text);
+    let recipients = $('#addresses-area').val();
+    console.warn(recipients);
     let x;
-    if (type != "email") {
+    if (type !== "email") {
         x = text.value;
     } else {
         x = "";
@@ -56,66 +28,159 @@ function mail(sendnow) {
     let wrap = {
         sendnow: sendnow,
         type: type,
-        templateText: templateText,
+        content: text,
         text: x,
         date: date,
         clientData: clientData
     };
     $.ajax({
         type: "POST",
-        url: "/client/mailing/send",
+        url: URL_POST_DATA,
         data: wrap,
         success: function (result) {
             console.log("success " + result);
         },
-        error: function (e) {
-            console.log("неверный формат записи, добавте clientData перед данными\n" + e);
+        error: function (error) {
+            console.log("неверный формат записи, добавте clientData перед данными\n" + error);
         }
     });
 }
 
+/**
+ * Функция, переключающая состояние кнопок режима рассылки и перенастраивающая интерфейс редактора.
+ * Для функции отправки сообщений посредством СМС, в CKEditor отключаются все плагины форматирования
+ * текста, так же, как и для отправки сообщений в Вк. Плюс меняется тип сообщения, messageType.
+ */
 $(document).ready(function () {
-    var nowDate = new Date();
-    var minutes = Math.ceil((nowDate.getMinutes() + 1) / 10) * 10;
-    var minDate = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), nowDate.getHours(), minutes, 0, 0);
-    var startDate = moment(minDate).utcOffset(180);
-    $('input[name="mailingDate"]').daterangepicker({
-        singleDatePicker: true,
-        timePicker: true,
-        timePickerIncrement: 10,
-        timePicker24Hour: true,
-        locale: {
-            format: 'DD.MM.YYYY HH:mm МСК'
-        },
-        minDate: startDate,
-        startDate: startDate
+    $("#message-type-button-group > button").click(function () {
+        if (messageType === $(this).attr("id")) {
+            return;
+        }
+
+        messageType = $(this).attr("id");
+
+        if (messageType !== 'email') {
+            ckeditorRemoveAllToolbars();
+            if (messageType === 'sms') {
+                $("#addresses-label").text(SEND_SMSS);
+            } else {
+                $("#addresses-label").text(SEND_TO_VK);
+            }
+        } else {
+            ckeditorAddAllToolbars();
+            $("#addresses-label").text(SEND_EMAILS);
+        }
+
+        $("#message-type-button-group > button").each(function (index, element) {
+            if ($(element).hasClass(BUTTON_INFO_CLASS)) {
+                $(element).removeClass(BUTTON_INFO_CLASS);
+                $(element).addClass(BUTTON_SECONDARY_CLASS);
+            }
+        });
+        $(this).addClass(BUTTON_INFO_CLASS);
     });
 });
 
-/*$(document).ready(function () {
-    editor.on('drop', function (e) {
-        var files = e.data.dataTransfer.getFile(0);
-        var reader = new FileReader();
-        reader.onload = function () {
-            if (CKEDITOR.instances["body1"].getData().includes("clientData")) {
-                editor.insertText("\n\n" + this.result);
-            } else {
-                editor.insertText("\nclientData\n" + this.result);
-            }
-        };
-        reader.readAsBinaryString(files);
+/**
+ * Функция, настраивающая datarangepicker
+ */
+$(document).ready(function () {
+    let startDate = moment(new Date()).utcOffset(180); //устанавливаем минимальную дату и время по МСК (UTC + 3 часа )
+    $('#messageSendingTime').daterangepicker({
+        "singleDatePicker": true, //отключаем выбор диапазона дат (range)
+        "showWeekNumbers": false,
+        "timePicker": true,
+        "timePicker24Hour": true,
+        "timePickerIncrement": 10,
+        "locale": {
+            "format": "DD.MM.YYYY HH:mm МСК",
+            "separator": " - ",
+            "applyLabel": "Apply",
+            "cancelLabel": "Cancel",
+            "fromLabel": "From",
+            "toLabel": "To",
+            "customRangeLabel": "Custom",
+            "weekLabel": "W",
+            "daysOfWeek": ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+            "monthNames": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            "firstDay": 0
+        },
+        "linkedCalendars": false,
+        "startDate": startDate,
+        "minDate": startDate //стартовая дата будет совпадать с минимальной
+    }, function (start, end, label) {
+        console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' +
+            end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
     });
-});*/
+});
 
-document.addEventListener('DOMContentLoaded', function () {
-    var dropZone1 = document.getElementById("clientData1");
-    dropZone1.addEventListener('drop', function (e) {
+//TODO Не срабатывает. Предназначен для установки текущей даты в стартовую и мин даты при открытии календаря
+$("#messageSendingTime").on('show.daterangepicker', function (event, picker) {
+    let minDate = moment(new Date()).utcOffset(180); //устанавливаем минимальную дату и время по МСК (UTC + 3 часа)
+    picker.minDate = minDate;
+    picker.startDate = minDate;
+});
+
+/**
+ * Заполнение блока адресов
+ */
+$(document).ready(function () {
+    $("#addresses-area").on("drop", function (event) {
         event.preventDefault();
-        var files = e.dataTransfer.files[0];
-        var reader = new FileReader();
-        reader.onload = function () {
-            dropZone1.value += "\n" + this.result;
+        event.stopPropagation();
+        $("#addresses-area").removeClass("drop-zone-is-dragover");
+        let reader = new FileReader();
+        let file = event.originalEvent.dataTransfer.files[0];
+        $("#file-info").text("Файл: " + file.name + ", Размер файла: " + file.size + " б");
+        reader.onload = function (event) {
+            let content = event.target.result;
+            $("#addresses-area").val(content.toLowerCase());
         };
-        reader.readAsBinaryString(files);
-    }, false);
-}, false);
+        reader.onerror = function (event) {
+            console.error("The file could not be read!" + event.target.error.code);
+            let fileInfo = $("#file-info");
+            //TODO переделать с проверкой, чтобы все было корректно
+            $(fileInfo).text("The file could not be read!" + event.target.error.code);
+            $(fileInfo).removeClass(BADGE_SUCCESS_CLASS);
+            $(fileInfo).addClass(BADGE_WARNING_CLASS);
+        };
+        reader.readAsText(file);
+    })
+});
+
+/**
+ * Начальная настройка CKEditor
+ */
+$(document).ready(function () {
+    CKEDITOR.addCss('.cke_editable p { margin: 0 !important; }');
+    CKEDITOR.replace(EDITOR, {
+        customConfig: '/ckeditor/add-all-toolbars.js'
+    });
+});
+
+function ckeditorAddAllToolbars() {
+    CKEDITOR.instances[EDITOR].destroy(true);
+    CKEDITOR.replace(EDITOR, {
+        customConfig: '/ckeditor/add-all-toolbars.js'
+    });
+}
+
+function ckeditorRemoveAllToolbars() {
+    CKEDITOR.instances[EDITOR].destroy(true);
+    CKEDITOR.replace(EDITOR, {
+        customConfig: '/ckeditor/remove-all-toolbars.js'
+    });
+}
+
+/**
+ * Визуализация событий dragover, dragleave, dragend, drop поля адресов
+ */
+$(document).ready(function () {
+    $("#addresses-area")
+        .on("dragover", function (event) {
+            $(this).addClass(DROP_ZONE_IS_DRAGOVER_CLASS);
+        })
+        .on("dragleave dragend drop", function () {
+            $(this).removeClass(DROP_ZONE_IS_DRAGOVER_CLASS);
+        })
+});
