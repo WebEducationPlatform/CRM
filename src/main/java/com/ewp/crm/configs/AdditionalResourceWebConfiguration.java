d42e14a5f41ee8c45a0858ffba67ee10dd674add
u <jek.kalashnikov@gmail.com>
2018-11-27 13:55:30 +0200

    test ckeditor img in jar

---
+++ src/main/java/com/ewp/crm/configs/AdditionalResourceWebConfiguration.java
@@ -0,0 +1,13 @@
+package com.ewp.crm.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").addResourceLocations("file:img/");
+    }
+}
