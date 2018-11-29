2678fb1c6230585550adf55ca4a1e09af924db1a
u <jek.kalashnikov@gmail.com>
2018-11-29 15:15:43 +0200

    fix bug whis emty message template

--- src/main/resources/static/js/main-table.js
+++ src/main/resources/static/js/main-table.js
@@ -959,6 +959,9 @@ $(function () {
         var templateId = $(this).data('templateId');
         var current = $(this);
         var currentStatus = $(this).prev('.send-custom-template');
+        if ($('#custom-eTemplate-body').val().length===0){
+            return currentStatus.text("Введите текст сообщения!");
+        }
         var formData = {
             clientId: clientId, templateId: templateId,
             body: $('#custom-eTemplate-body').val()
