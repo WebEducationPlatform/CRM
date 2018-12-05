0d505c7311c0b99b0f380d912182dec2e7ce8391
u <jek.kalashnikov@gmail.com>
2018-12-05 15:20:07 +0200

    removed chance for create not required in create require form modal in settings VK form

--- src/main/resources/static/js/vk-request-form.js
+++ src/main/resources/static/js/vk-request-form.js
@@ -34,13 +34,11 @@ function createVkRequestField() {
         }
     }
 
-    var selectIndex = document.getElementById("app-type-create").options.selectedIndex;
-    var typeRequest = document.getElementById("app-type-create").options[selectIndex].text;
     var url = "/vk/request/create";
     var mass = {
         numberVkField: rowCount,
         nameVkField: nameRequest,
-        typeVkField: typeRequest
+        typeVkField: "Обязательное"
     };
     var emp = JSON.stringify(mass);
     $.ajax({
@@ -163,6 +161,9 @@ function updateVkRequestField() {
     var id = document.getElementById("app-id").value;
     var indexForNameRequest = document.getElementById("app-name").options.selectedIndex;
     var nameRequest = document.getElementById("app-name").options[indexForNameRequest].text;
+    if (nameRequest === "") {
+        return;
+    }
     for (var i = 1; i < table.rows.length; i++) {
         var row = table.rows[i];
         var name = row.cells[1].textContent;
