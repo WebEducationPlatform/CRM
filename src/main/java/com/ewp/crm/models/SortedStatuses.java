1a87ec8ec1b207e37965653b0f8d60037c70dda8
yaegorko <yaegorko@gmail.com>
2018-12-06 01:26:46 +0400

    try to start

---
+++ src/main/java/com/ewp/crm/models/SortedStatuses.java
@@ -0,0 +1,65 @@
+package com.ewp.crm.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "SortedStatuses")
+@Table(name = "sorted_statuses")
+public class SortedStatuses implements Serializable {
+
+    @EmbeddedId
+    private SortedStatusesId sortedStatusesId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("statusId")
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
+    private User user;
+
+    @Column(name = "sortingType")
+    private String sortingType;
+
+    public SortedStatuses() {
+    }
+
+    public SortedStatuses(Status status, User user) {
+        this.status = status;
+        this.user = user;
+        this.sortedStatusesId = new SortedStatusesId(status.getId(), user.getId());
+
+    }
+
+    public SortedStatusesId getSortedStatusesId() {
+        return sortedStatusesId;
+    }
+
+    public void setSortedStatusesId(SortedStatusesId sortedStatusesId) {
+        this.sortedStatusesId = sortedStatusesId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getSortingType() {
+        return sortingType;
+    }
+
+    public void setSortingType(String sortingType) {
+        this.sortingType = sortingType;
+    }
+}
