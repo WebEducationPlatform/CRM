6e41813f1d7ea02631bd588c58f9f1be30d430cb
yaegorko <yaegorko@gmail.com>
2018-12-13 20:22:35 +0400

    c style

--- src/main/java/com/ewp/crm/service/impl/ClientHistoryServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/ClientHistoryServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
