6ab10b30cbd7103c9ea6e0abc47ecc09fe00dbcf
denisgrebenuk <dencheg995@gmail.com>
2018-12-19 18:28:43 +0300

    captcha

--- src/main/java/com/ewp/crm/CrmApplication.java
+++ src/main/java/com/ewp/crm/CrmApplication.java
@@ -1,18 +1,16 @@
 package com.ewp.crm;
 
 import com.ewp.crm.configs.initializer.DataInitializer;
-import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import java.io.IOException;
 
 @SpringBootApplication
 public class CrmApplication {
 
-	public static void main(String[] args) throws IOException, JSONException, InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
-    }
+	}
 
 	@Bean(initMethod = "init")
 	public DataInitializer initTestData() {
