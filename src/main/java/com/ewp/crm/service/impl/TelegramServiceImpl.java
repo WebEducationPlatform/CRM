c874cabddf54bf35dae0771ae968d482f5ac5175
shonorov <shonorov@gmail.com>
2018-12-21 16:40:26 +0300

    It starts

--- src/main/java/com/ewp/crm/service/impl/TelegramServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/TelegramServiceImpl.java
@@ -319,14 +319,18 @@ public class TelegramServiceImpl implements TelegramService, JMConversation {
     }
 
     @Override
-    public Interlocutor getMe() {
-        TdApi.User user = getTgMe();
-        return tdlibUserToInterlocutor(user);
+    public Optional<Interlocutor> getMe() {
+        Optional<Interlocutor> result = Optional.empty();
+        Optional<TdApi.User> user = Optional.of(getTgMe());
+        if (user.isPresent()) {
+            result = Optional.of(tdlibUserToInterlocutor(user.get()));
+        }
+        return result;
     }
 
     private ChatMessage tdlibMessageToChatMessage(TdApi.Message message) {
         ZonedDateTime time = ZonedDateTime.ofInstant(Instant.ofEpochMilli(message.date), TimeZone.getDefault().toZoneId());
-        return new ChatMessage(message.id, String.valueOf(message.chatId), ChatType.telegram, message.content.toString(), time, false, true);
+        return new ChatMessage(String.valueOf(message.id), String.valueOf(message.chatId), ChatType.telegram, message.content.toString(), time, false, true);
     }
 
     private List<ChatMessage> tdlibMessagesToChatMessages(TdApi.Messages messages) {
