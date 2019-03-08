d46b1858500143aca3dd006135e883704687bb63
Vladimir Yarovoy <shpi0@yandex.ru>
2019-03-08 14:46:59 +0300

    Saving user's filter preferences for all student page. Rejected student hides in table according to the filter settings.

--- src/main/java/com/ewp/crm/controllers/StudentController.java
+++ src/main/java/com/ewp/crm/controllers/StudentController.java
@@ -1,12 +1,16 @@
 package com.ewp.crm.controllers;
 
+import com.ewp.crm.models.Status;
+import com.ewp.crm.models.User;
 import com.ewp.crm.service.interfaces.MessageTemplateService;
+import com.ewp.crm.service.interfaces.ProjectPropertiesService;
 import com.ewp.crm.service.interfaces.StatusService;
 import com.ewp.crm.service.interfaces.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +26,28 @@ public class StudentController {
     private final StudentService studentService;
     private final StatusService statusService;
     private final MessageTemplateService messageTemplateService;
+    private final ProjectPropertiesService projectPropertiesService;
 
     @Autowired
-    public StudentController(StudentService studentService, StatusService statusService, MessageTemplateService messageTemplateService) {
+    public StudentController(StudentService studentService, StatusService statusService,
+                             MessageTemplateService messageTemplateService, ProjectPropertiesService projectPropertiesService) {
         this.studentService = studentService;
         this.statusService = statusService;
         this.messageTemplateService = messageTemplateService;
+        this.projectPropertiesService = projectPropertiesService;
     }
 
     @GetMapping(value = "/all")
-    public ModelAndView showAllStudents() {
+    public ModelAndView showAllStudents(@AuthenticationPrincipal User userFromSession) {
         ModelAndView modelAndView = new ModelAndView("all-students-table");
+        modelAndView.addObject("filters", userFromSession.getStudentPageFilters() != null ? userFromSession.getStudentPageFilters() : "");
+        Long defaultStatusId = projectPropertiesService.getOrCreate().getClientRejectStudentStatus();
+        if (defaultStatusId != null) {
+            Status status = statusService.get(defaultStatusId);
+            modelAndView.addObject("defaultStatusForRejectedStudent", status.getName());
+        } else {
+            modelAndView.addObject("defaultStatusForRejectedStudent", "");
+        }
         modelAndView.addObject("students", studentService.getAll());
         modelAndView.addObject("statuses", statusService.getAll());
         modelAndView.addObject("emailTmpl", messageTemplateService.getAll());
