eff4998e1e75ff2afdc43cd7d695d20c8361f50d
Nikolay <mailto:miqolay@gmail.com>
2019-03-15 02:34:04 +0500

    vk balance getting repair

--- src/main/java/com/ewp/crm/controllers/rest/VkRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/VkRestController.java
@@ -9,6 +9,13 @@ import com.ewp.crm.service.interfaces.MessageTemplateService;
 import com.ewp.crm.service.interfaces.VKService;
 import com.ewp.crm.service.interfaces.VkMemberService;
 import com.ewp.crm.service.interfaces.VkTrackedClubService;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +40,8 @@ import com.vk.api.sdk.queries.ads.AdsGetStatisticsQuery;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.core.env.Environment;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -174,11 +183,12 @@ public class VkRestController {
 		UserAuthResponse authResponse = vk.oauth()
 				.userAuthorizationCodeFlow(Integer.valueOf(clientId), clientSecret, "http://localhost:" + serverPort + "/rest/vkontakte/ads", code)
 				.execute();
+		String accessToken = authResponse.getAccessToken();
         Long clicks = 0L;
         String spent = "";
         String budget = "";
 
-		UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
+		UserActor actor = new UserActor(authResponse.getUserId(), accessToken);
 		AdsGetBudgetQuery advertisement = vk.ads().getBudget(actor, Integer.parseInt(accountId));
 		JSONObject balance = new JSONObject(advertisement.executeAsString());
 		budget = balance.getString("response");
@@ -187,6 +197,49 @@ public class VkRestController {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		SimpleDateFormat simpleDate = new SimpleDateFormat("dd.MM.yyyy");
 		String dateToFrom = simpleDateFormat.format(date);
+
+        String uriBudget = "https://api.vk.com/method/" + "ads.getBudget" +
+                "?account_id=" + "1605137078" +
+                "&version=" + "5.78" +
+                "&access_token=" + "524fb23e95f0049733785f33b6f33b09d7cb397d40c03d2a08fadc84dcbab2834452908c75719a7a56a2f";
+        HttpGet httpGetBudget = new HttpGet(uriBudget);
+        HttpClient httpClientBudget = getHttpClient();
+        HttpResponse responseBudget = null;
+        JSONObject jsonBudget = null;
+        try {
+            responseBudget = httpClientBudget.execute(httpGetBudget);
+            String resultStat = EntityUtils.toString(responseBudget.getEntity());
+            jsonBudget = new JSONObject(resultStat);
+            System.out.println("jsonBudget extracted = " + jsonBudget.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        String uriStat = "https://api.vk.com/method/" + "ads.getStatistics" +
+                "?account_id=" + "1605137078" +
+                "&ids_type=" + "office" +
+                "&ids=" + "1605137078" +
+                "&period=" + "day" +
+                "&date_from=" + "2019-03-15" +
+                "&date_to=" + "2019-03-15" +
+                "&version=" + "5.78" +
+                "&access_token=" + "524fb23e95f0049733785f33b6f33b09d7cb397d40c03d2a08fadc84dcbab2834452908c75719a7a56a2f";
+
+        HttpGet httpGetStat = new HttpGet(uriStat);
+        HttpClient httpClientStat = getHttpClient();
+        HttpResponse responseStat = null;
+        JSONObject jsonStat = null;
+        try {
+            responseStat = httpClientStat.execute(httpGetStat);
+            String resultStat = EntityUtils.toString(responseStat.getEntity());
+            jsonStat = new JSONObject(resultStat);
+            System.out.println("jsonStat extracted = " + jsonStat.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 		AdsGetStatisticsQuery advertisement3 = vk.ads().getStatistics(actor,
 				Integer.parseInt(accountId),
 				AdsGetStatisticsIdsType.OFFICE,
@@ -195,6 +248,9 @@ public class VkRestController {
 				dateToFrom,
 				dateToFrom);
 		String jString = new JSONObject(advertisement3.executeAsString()).toString();
+
+
+
         JSONArray responce = new JSONObject(jString).getJSONArray("response");
         StringBuilder statStr = new StringBuilder("Статистика по вк-рекламному кабинету:").append(System.lineSeparator());
         statStr.append("Дата: ").append(simpleDate.format(date)).append(System.lineSeparator());
@@ -219,5 +275,12 @@ public class VkRestController {
         System.out.println(statStr);
 		return statStr.toString();
 	}
+
+    public HttpClient getHttpClient() {
+        return HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+    }
 
 }
