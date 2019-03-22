50f6ef1b8f893e6050103723690561bc1958334b
yogo96 <yogoman96@mail.ru>
2019-03-22 18:36:19 +0300

    add contract config prop

---
+++ src/main/java/com/ewp/crm/configs/inteface/ContractConfig.java
@@ -0,0 +1,16 @@
+package com.ewp.crm.configs.inteface;
+
+public interface ContractConfig {
+
+    String getName();
+
+    String getPath();
+
+    String getMonth();
+
+    String getMonthPoint();
+
+    String getOnetimePoint();
+
+    String getDiploma();
+}
