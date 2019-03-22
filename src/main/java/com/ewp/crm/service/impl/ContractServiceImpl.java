edd74ed9b5bf13e6bc23d99b987b4065f6900a06
yogo96 <yogoman96@mail.ru>
2019-03-23 01:46:16 +0300

    change methods

--- src/main/java/com/ewp/crm/service/impl/ContractServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/ContractServiceImpl.java
@@ -1,12 +1,13 @@
-package com.ewp.crm.service.google;
+package com.ewp.crm.service.impl;
 
 import com.ewp.crm.configs.GoogleAPIConfigImpl;
 import com.ewp.crm.configs.inteface.ContractConfig;
 import com.ewp.crm.models.ContractDataForm;
 import com.ewp.crm.models.ContractSetting;
+import com.ewp.crm.models.GoogleToken;
 import com.ewp.crm.models.ProjectProperties;
-import com.ewp.crm.repository.interfaces.GoogleTokenRepository;
 import com.ewp.crm.service.interfaces.ContractService;
+import com.ewp.crm.service.interfaces.GoogleTokenService;
 import com.ewp.crm.service.interfaces.ProjectPropertiesService;
 import com.ibm.icu.text.Transliterator;
 import org.apache.http.HttpResponse;
@@ -42,124 +43,152 @@ public class ContractServiceImpl implements ContractService {
     private static Logger logger = LoggerFactory.getLogger(ContractServiceImpl.class);
 
     private final static String CYRILLIC_TO_LATIN = "Russian-Latin/BGN";
+    private final String uploadUri;
+    private final String updateUri;
 
     private final ProjectPropertiesService projectPropertiesService;
-    private final GoogleTokenRepository googleTokenRepository;
+    private final GoogleTokenService googleTokenService;
     private final ContractConfig contractConfig;
-    private final GoogleAPIConfigImpl googleAPIConfig;
 
     @Autowired
-    public ContractServiceImpl(ProjectPropertiesService projectPropertiesService, GoogleTokenRepository googleTokenRepository, ContractConfig contractConfig, GoogleAPIConfigImpl googleAPIConfig) {
+    public ContractServiceImpl(ProjectPropertiesService projectPropertiesService, GoogleTokenService googleTokenService, ContractConfig contractConfig, GoogleAPIConfigImpl googleAPIConfig) {
         this.projectPropertiesService = projectPropertiesService;
-        this.googleTokenRepository = googleTokenRepository;
+        this.googleTokenService = googleTokenService;
         this.contractConfig = contractConfig;
-        this.googleAPIConfig = googleAPIConfig;
+        this.uploadUri = googleAPIConfig.getDriveUploadUri();
+        this.updateUri = googleAPIConfig.getDriveUpdateUri();
     }
 
     @Override
     public Optional<String> getContractIdByFormDataWithSetting(ContractDataForm data, ContractSetting setting) {
-        Optional<File> fileOptional = getFileWithDataAndSetting(data, setting);
+        Optional<File> fileOptional = createFileWithDataAndSetting(data, setting);
         if (fileOptional.isPresent()) {
-            try {
+            Optional<GoogleToken> googleTokenOptional = googleTokenService.getRefreshedToken();
+            if (googleTokenOptional.isPresent()) {
                 File file = fileOptional.get();
-                String token = googleTokenRepository.getOne(1L).getValue();
-
-                String uri = googleAPIConfig.getDriveUploadUri() +
-                        "?uploadType=media&" +
-                        "access_token=" + token;
-
-                HttpPost httpPostMessages = new HttpPost(uri);
-                httpPostMessages.setHeader("Content-type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-                EntityBuilder builder = EntityBuilder.create();
-                builder.setFile(file);
-                httpPostMessages.setEntity(builder.build());
+                String token = googleTokenOptional.get().getAccessToken();
                 HttpClient httpClient = getHttpClient();
-                HttpResponse response = httpClient.execute(httpPostMessages);
-                String res = EntityUtils.toString(response.getEntity());
-                JSONObject json = new JSONObject(res);
-                String id = json.getString("id");
+                Optional<String> optionalId = Optional.empty();
 
-                String updateNameUri = googleAPIConfig.getDriveUpdateUri();
-
-                HttpPatch httpPatch = new HttpPatch(updateNameUri + id + "?access_token=" + token);
-                httpPatch.setHeader("Content-type", "application/json");
-                String fileName = "{ \"name\":\"" + file.getName().replaceAll("ʹ", "") + "\"}";
-                httpPatch.setEntity(new StringEntity(fileName));
-                httpClient.execute(httpPatch);
-
-                HttpPost httpPost = new HttpPost(updateNameUri + id + "/permissions?access_token=" + token);
-                httpPost.setHeader("Content-type", "application/json");
-                String permission =
-                        "{" +
-                                "  \"role\": \"reader\"," +
-                                "  \"type\": \"anyone\" " +
-                                "}";
-                httpPost.setEntity(new StringEntity(permission));
-                httpClient.execute(httpPost);
-                file.delete();
-                return Optional.of(id);
-            } catch (IOException | JSONException e) {
-                logger.error("Error with upload file to Google Drive");
+                String id = uploadFileAndGetFileId(file, token, httpClient);
+                if (!id.isEmpty()) {
+                    updateFileNameOnGoogleDrive(id, file.getName(), token, httpClient);
+                    uploadFileAccessOnGoogleDrive(id, token, httpClient);
+                    optionalId = Optional.of(id);
+                }
+                if (file.delete()) {
+                    logger.info("File deleting " + file.getName());
+                }
+                return optionalId;
             }
         }
         return Optional.empty();
     }
 
-    private Optional<File> getFileWithDataAndSetting(ContractDataForm data, ContractSetting setting) {
-        if (!googleTokenRepository.getOne(1L).getValue().isEmpty()) {
-            try {
-                String templatePath = contractConfig.getFilePath();
-                String templateName = contractConfig.getFileName();
-                WordprocessingMLPackage mlp = WordprocessingMLPackage.load(new File(templatePath + templateName));
-                HashMap<String, String> map = new HashMap<>();
-                VariablePrepare.prepare(mlp);
-                Date currentDate = new Date();
-                String dateStr = new SimpleDateFormat("dd MM yyyy").format(currentDate);
-                ProjectProperties projectProperties = projectPropertiesService.get();
-                long lastId = projectProperties.getContractLastId();
-                lastId++;
-                map.put("contract-number", String.valueOf(lastId));
-                map.put("date", dateStr);
-                map.put("name", data.getInputLastName() + " " + data.getInputFirstName() + " " + data.getInputMiddleName());
-                map.put("passport-series", data.getPassportData().getSeries());
-                map.put("passport-number", data.getPassportData().getNumber());
-                map.put("passport-issued", data.getPassportData().getIssuedBy());
-                map.put("passport-date", data.getPassportData().getDateOfIssue());
-                map.put("passport-registration", data.getPassportData().getIssuedBy());
-                map.put("birthday", data.getInputBirthday());
-                map.put("email", data.getInputEmail());
-                map.put("phone-number", data.getInputPhoneNumber());
-                if (!setting.isOneTimePayment()) {
-                    map.put("onetime", contractConfig.getMonth());
-                    map.put("point", contractConfig.getMonthPoint());
-                } else {
-                    map.put("onetime", "");
-                    map.put("point", contractConfig.getOnetimePoint());
-                }
-                if (setting.isDiploma()) {
-                    map.put("diploma", contractConfig.getDiploma());
-                } else {
-                    map.put("diploma", "");
-                }
-                map.put("summa", setting.getPaymentAmount());
-                mlp.getMainDocumentPart().variableReplace(map);
-                String fileName = renameFileToLatin(data);
-                File file = new File(templatePath + fileName + ".docx");
-                file.createNewFile();
-                Docx4J.save(mlp, file);
-                projectProperties.setContractLastId(lastId);
-                projectPropertiesService.update(projectProperties);
-                return Optional.of(file);
-            } catch (Exception e) {
-                logger.info("Error with create file");
+    private String uploadFileAndGetFileId(File file, String token, HttpClient httpClient) {
+        try {
+            String uri = uploadUri +
+                    "?uploadType=media&" +
+                    "access_token=" + token;
+
+            HttpPost httpPostMessages = new HttpPost(uri);
+            httpPostMessages.setHeader("Content-type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            EntityBuilder builder = EntityBuilder.create();
+            builder.setFile(file);
+            httpPostMessages.setEntity(builder.build());
+            HttpResponse response = httpClient.execute(httpPostMessages);
+            String res = EntityUtils.toString(response.getEntity());
+            JSONObject json = new JSONObject(res);
+            return json.getString("id");
+        } catch (IOException e) {
+            logger.error("Error load file", e);
+        } catch (JSONException e) {
+            logger.error("Error parsing json", e);
+        }
+        return "";
+    }
+
+    private void updateFileNameOnGoogleDrive(String id, String fileName, String token, HttpClient httpClient) {
+        try {
+            HttpPatch httpPatch = new HttpPatch(updateUri + id + "?access_token=" + token);
+            httpPatch.setHeader("Content-type", "application/json");
+            String jsonUpdateName = "{ \"name\":\"" + fileName + "\"}";
+            httpPatch.setEntity(new StringEntity(jsonUpdateName));
+            httpClient.execute(httpPatch);
+        } catch (IOException e) {
+            logger.error("Error with upload jsonEntity", e);
+        }
+    }
+
+    private void uploadFileAccessOnGoogleDrive(String id, String token, HttpClient httpClient) {
+        try {
+            HttpPost httpPost = new HttpPost(updateUri + id + "/permissions?access_token=" + token);
+            httpPost.setHeader("Content-type", "application/json");
+            String permission = "{" +
+                    "  \"role\": \"reader\"," +
+                    "  \"type\": \"anyone\" " +
+                    "}";
+            httpPost.setEntity(new StringEntity(permission));
+            httpClient.execute(httpPost);
+        } catch (IOException e) {
+            logger.error("Error with upload jsonEntity", e);
+        }
+    }
+
+    private Optional<File> createFileWithDataAndSetting(ContractDataForm data, ContractSetting setting) {
+        try {
+            String templatePath = contractConfig.getFilePath();
+            String templateName = contractConfig.getFileName();
+            WordprocessingMLPackage mlp = WordprocessingMLPackage.load(new File(templatePath + templateName));
+            HashMap<String, String> map = new HashMap<>();
+            VariablePrepare.prepare(mlp); // throws Exception
+            ProjectProperties projectProperties = projectPropertiesService.get();
+            Long lastId = projectProperties.getContractLastId();
+
+            map.put("contract-number", String.valueOf(++lastId));
+            map.put("date", new SimpleDateFormat("dd MM yyyy").format(new Date()));
+            map.put("name", data.getInputLastName() + " " + data.getInputFirstName() + " " + data.getInputMiddleName());
+            map.put("passport-series", data.getPassportData().getSeries());
+            map.put("passport-number", data.getPassportData().getNumber());
+            map.put("passport-issued", data.getPassportData().getIssuedBy());
+            map.put("passport-date", data.getPassportData().getDateOfIssue());
+            map.put("passport-registration", data.getPassportData().getIssuedBy());
+            map.put("birthday", data.getInputBirthday());
+            map.put("email", data.getInputEmail());
+            map.put("phone-number", data.getInputPhoneNumber());
+            if (!setting.isOneTimePayment()) {
+                map.put("onetime", contractConfig.getMonth());
+                map.put("point", contractConfig.getMonthPoint());
+            } else {
+                map.put("onetime", "");
+                map.put("point", contractConfig.getOnetimePoint());
             }
+            if (setting.isDiploma()) {
+                map.put("diploma", contractConfig.getDiploma());
+            } else {
+                map.put("diploma", "");
+            }
+            map.put("summa", setting.getPaymentAmount());
+
+            mlp.getMainDocumentPart().variableReplace(map);
+            File file = new File(templatePath + renameFileToLatin(data) + contractConfig.getFormat());
+            if (file.createNewFile()) {
+                logger.info("Creating file " + file.getName());
+            }
+            Docx4J.save(mlp, file);
+            projectProperties.setContractLastId(lastId);
+            projectPropertiesService.update(projectProperties);
+            return Optional.of(file);
+        } catch (Exception e) {
+            logger.info("Error with create file", e);
         }
         return Optional.empty();
     }
 
     private String renameFileToLatin(ContractDataForm data) {
         Transliterator toLatinTrans = Transliterator.getInstance(CYRILLIC_TO_LATIN);
-        return toLatinTrans.transliterate(data.getInputLastName() + data.getInputFirstName());
+        String fileName = toLatinTrans.transliterate(data.getInputLastName() + data.getInputFirstName());
+        return fileName.replaceAll("ʹ", "");
     }
 
     private HttpClient getHttpClient() {
