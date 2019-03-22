23d341e3d056990072cd28b9eed5330a3da76d51
Vladimir Yarovoy <shpi0@yandex.ru>
2019-03-22 18:22:56 +0300

    Remember me logged in

---
+++ src/main/java/com/ewp/crm/repository/impl/PersistentLoginRepositoryImpl.java
@@ -0,0 +1,62 @@
+package com.ewp.crm.repository.impl;
+
+import com.ewp.crm.models.PersistentLogin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Date;
+
+@Repository
+@Transactional
+public class PersistentLoginRepositoryImpl implements PersistentTokenRepository {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public PersistentLoginRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void createNewToken(PersistentRememberMeToken persistentRememberMeToken) {
+        PersistentLogin login = new PersistentLogin();
+        login.setUsername(persistentRememberMeToken.getUsername());
+        login.setSeries(persistentRememberMeToken.getSeries());
+        login.setToken(persistentRememberMeToken.getTokenValue());
+        login.setLastUsed(persistentRememberMeToken.getDate());
+        entityManager.persist(login);
+        entityManager.flush();
+    }
+
+    @Override
+    public void updateToken(String s, String tokenValue, Date date) {
+        PersistentLogin login = entityManager.createQuery("SELECT pl FROM PersistentLogin pl WHERE pl.series = :seriesId", PersistentLogin.class)
+                .setParameter("seriesId", s)
+                .getSingleResult();
+        login.setToken(tokenValue);
+        login.setLastUsed(date);
+        entityManager.flush();
+    }
+
+    @Override
+    public PersistentRememberMeToken getTokenForSeries(String s) {
+        PersistentLogin login = entityManager.createQuery("SELECT pl FROM PersistentLogin pl WHERE pl.series = :seriesId", PersistentLogin.class)
+                .setParameter("seriesId", s)
+                .getSingleResult();
+        if (login != null) {
+            return new PersistentRememberMeToken(login.getUsername(), login.getSeries(), login.getToken(), login.getLastUsed());
+        }
+        return null;
+    }
+
+    @Override
+    public void removeUserTokens(String s) {
+        entityManager.createQuery("DELETE FROM PersistentLogin pl WHERE pl.series = :seriesId")
+                .setParameter("seriesId", s)
+                .executeUpdate();
+    }
+}
