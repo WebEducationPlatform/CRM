8506aa4dd97ec3c15e3a67fa8313d4f788837494
yogo96 <yogoman96@mail.ru>
2019-03-23 01:46:05 +0300

    change methods

--- src/main/java/com/ewp/crm/controllers/ContractController.java
+++ src/main/java/com/ewp/crm/controllers/ContractController.java
@@ -1,10 +1,13 @@
 package com.ewp.crm.controllers;
 
-import com.ewp.crm.models.ContractFormData;
-import com.ewp.crm.service.interfaces.ClientService;
-import com.ewp.crm.service.interfaces.GoogleDriveService;
+import com.ewp.crm.configs.GoogleAPIConfigImpl;
+import com.ewp.crm.models.*;
+import com.ewp.crm.service.interfaces.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,28 +17,62 @@ import java.util.Optional;
 @RequestMapping("/contract")
 public class ContractController {
 
-    private final GoogleDriveService googleDriveService;
+    private static final Logger logger = LoggerFactory.getLogger(ContractController.class);
+
+    private final ContractService contractService;
     private final ClientService clientService;
+    private final ContractSettingService contractSettingService;
+    private final ProjectPropertiesService projectPropertiesService;
+    private final MailSendService mailSendService;
+    private final GoogleAPIConfigImpl googleAPIConfig;
+
 
     @Autowired
-    public ContractController(GoogleDriveService googleDriveService, ClientService clientService) {
-        this.googleDriveService = googleDriveService;
+    public ContractController(ContractService contractService, ClientService clientService, ContractSettingService contractSettingService, ProjectPropertiesService projectPropertiesService, MailSendService mailSendService, GoogleAPIConfigImpl googleAPIConfig) {
+        this.contractService = contractService;
         this.clientService = clientService;
+        this.contractSettingService = contractSettingService;
+        this.projectPropertiesService = projectPropertiesService;
+        this.mailSendService = mailSendService;
+        this.googleAPIConfig = googleAPIConfig;
     }
 
-    @GetMapping
-    public ModelAndView completeForm(@RequestParam Long id) {
-        ModelAndView model = new ModelAndView("contract");
-        model.addObject("clientId", id);
-        model.addObject("data", new ContractFormData());
-        return model;
+    @GetMapping("/{hash}")
+    public ModelAndView completeForm(@PathVariable("hash") String hash) {
+        if (contractSettingService.existsByHash(hash)) {
+            ModelAndView model = new ModelAndView("contract");
+            model.addObject("hash", hash);
+            model.addObject("data", new ContractDataForm());
+            return model;
+        }
+        return new ModelAndView();
     }
 
-    @PostMapping
-    public String response(@RequestParam Long id, @ModelAttribute ContractFormData data) {
-        clientService.updateClientByIdFromContractForm(id, data);
-        Optional<String> contractId = googleDriveService.createContractWithData(data);
-        //ИСПРАВИТЬ!
-        return contractId.map(fileId -> "redirect:https://docs.google.com/document/d/" + fileId + "/edit?usp=sharing").orElse("redirect:https://vk.com/yogo143");
+    @Transactional
+    @PostMapping("/{hash}")
+    public String response(@PathVariable("hash") String hash, @ModelAttribute ContractDataForm data) {
+        //Есть одноразовая ссылка?
+        if (contractSettingService.existsByHash(hash)) {
+            //Настройки договора из Бд
+            if (contractSettingService.getByHash(hash).isPresent()) {
+                ContractSetting setting = contractSettingService.getByHash(hash).get();
+                Long clientId = setting.getClientId();
+                //Работа с договором и получение ссылки на него
+                Optional<String> contractId = contractService.getContractIdByFormDataWithSetting(data, setting);
+                if (contractId.isPresent()) {
+                    clientService.updateClientFromContractForm(clientId, data);
+                    String docLink = googleAPIConfig.getDocsUri() + contractId.get() + "/edit?usp=sharing";
+                    clientService.setContractLink(clientId, docLink);
+                    ProjectProperties current = projectPropertiesService.get();
+                    if (current.getContractTemplate() != null) {
+                        mailSendService.prepareAndSend(clientId, current.getContractTemplate().getTemplateText(), "", null);
+                    }
+                    contractSettingService.deleteByHash(hash);
+                    return "redirect:" + docLink;
+                }
+            }
+        }
+        //TODO страница ошибок
+        return "";
     }
 }
