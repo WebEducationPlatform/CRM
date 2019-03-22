4e6f80727a57bdb0af67c11d8060d925916204d8
yogo96 <yogoman96@mail.ru>
2019-03-22 18:36:45 +0300

    rename

--- src/main/java/com/ewp/crm/service/google/ContractServiceImpl.java
+++ src/main/java/com/ewp/crm/service/google/ContractServiceImpl.java
@@ -1,10 +1,11 @@
 package com.ewp.crm.service.google;
 
-import com.ewp.crm.models.ContractFormData;
-import com.ewp.crm.models.LastContractId;
+import com.ewp.crm.models.ContractDataForm;
+import com.ewp.crm.models.ContractSetting;
+import com.ewp.crm.models.ProjectProperties;
 import com.ewp.crm.repository.interfaces.GoogleTokenRepository;
-import com.ewp.crm.repository.interfaces.LastContractIdRepository;
 import com.ewp.crm.service.interfaces.GoogleDriveService;
+import com.ewp.crm.service.interfaces.ProjectPropertiesService;
 import com.ibm.icu.text.Transliterator;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -38,30 +39,30 @@ public class GoogleDriveServiceImpl implements GoogleDriveService {
 
     private static Logger logger = LoggerFactory.getLogger(GoogleDriveServiceImpl.class);
 
-    private final static String templateFileUrl = "contract-template.docx";
-    private final static String templatePathUrl = "src/main/resources/";
-    private final static String uploadUrl = "https://www.googleapis.com/upload/drive/v3/files?";
-    private final static String updateNameUrl = "https://www.googleapis.com/drive/v3/files/";
+    private final static String TEMPLATE_FILE_URL = "contract-template.docx";
+    private final static String TEMPLATE_PATH_URL = "src/main/resources/";
+    private final static String UPLOAD_URL = "https://www.googleapis.com/upload/drive/v3/files";
+    private final static String UPDATE_NAME_URL = "https://www.googleapis.com/drive/v3/files/";
     private final static String CYRILLIC_TO_LATIN = "Russian-Latin/BGN";
 
-    private final LastContractIdRepository lastContractIdRepository;
+    private final ProjectPropertiesService projectPropertiesService;
     private final GoogleTokenRepository googleTokenRepository;
 
     @Autowired
-    public GoogleDriveServiceImpl(LastContractIdRepository lastContractIdRepository, GoogleTokenRepository googleTokenRepository) {
-        this.lastContractIdRepository = lastContractIdRepository;
+    public GoogleDriveServiceImpl(ProjectPropertiesService projectPropertiesService, GoogleTokenRepository googleTokenRepository) {
+        this.projectPropertiesService = projectPropertiesService;
         this.googleTokenRepository = googleTokenRepository;
     }
 
     @Override
-    public Optional<String> createContractWithData(ContractFormData data) {
-        Optional<File> fileOptional = getFileWithData(data);
+    public Optional<String> getContractIdByFormDataWithSetting(ContractDataForm data, ContractSetting setting) {
+        Optional<File> fileOptional = getFileWithDataAndSetting(data,setting);
         if (fileOptional.isPresent()) {
             try {
                 File file = fileOptional.get();
                 String token = googleTokenRepository.getOne(1L).getValue();
 
-                String uri = uploadUrl + "uploadType=media&" +
+                String uri = UPLOAD_URL + "?uploadType=media&" +
                         "access_token=" + token;
 
                 HttpPost httpPostMessages = new HttpPost(uri);
@@ -75,13 +76,13 @@ public class GoogleDriveServiceImpl implements GoogleDriveService {
                 JSONObject json = new JSONObject(res);
                 String id = json.getString("id");
 
-                HttpPatch httpPatch = new HttpPatch(updateNameUrl + id + "?access_token=" + token);
+                HttpPatch httpPatch = new HttpPatch(UPDATE_NAME_URL + id + "?access_token=" + token);
                 httpPatch.setHeader("Content-type", "application/json");
-                String fileName = "{ \"name\":\"" + file.getName().replaceAll("[ʹ\\s]","") + "\"}";
+                String fileName = "{ \"name\":\"" + file.getName().replaceAll("ʹ","") + "\"}";
                 httpPatch.setEntity(new StringEntity(fileName));
                 httpClient.execute(httpPatch);
 
-                HttpPost httpPost = new HttpPost(updateNameUrl + id + "/permissions?access_token=" + token);
+                HttpPost httpPost = new HttpPost(UPDATE_NAME_URL + id + "/permissions?access_token=" + token);
                 httpPost.setHeader("Content-type", "application/json");
                 String permission = "{" +
                         "  \"role\": \"reader\"," +
@@ -98,21 +99,20 @@ public class GoogleDriveServiceImpl implements GoogleDriveService {
         return Optional.empty();
     }
 
-    private Optional<File> getFileWithData(ContractFormData data) {
+    private Optional<File> getFileWithDataAndSetting(ContractDataForm data, ContractSetting setting) {
         if (!googleTokenRepository.getOne(1L).getValue().isEmpty()) {
             try {
-                WordprocessingMLPackage mlp = WordprocessingMLPackage.load(new File(templatePathUrl + templateFileUrl));
+                WordprocessingMLPackage mlp = WordprocessingMLPackage.load(new File(TEMPLATE_PATH_URL + TEMPLATE_FILE_URL));
                 HashMap<String, String> map = new HashMap<>();
                 VariablePrepare.prepare(mlp);
                 Date currentDate = new Date();
                 String dateStr = new SimpleDateFormat("dd MM yyyy").format(currentDate);
-                LastContractId lastContractId = lastContractIdRepository.getById(1L);
-                long lastId = lastContractId.getLastId();
+                ProjectProperties projectProperties = projectPropertiesService.get();
+                long lastId = projectProperties.getContractLastId();
                 lastId++;
-                lastContractId.setLastId(lastId);
                 map.put("contract-number", String.valueOf(lastId));
                 map.put("date", dateStr);
-                map.put("name", data.getInputName());
+                map.put("name", data.getInputLastName() + " " + data.getInputFirstName() + " " + data.getInputMiddleName());
                 map.put("passport-series", data.getPassportData().getSeries());
                 map.put("passport-number", data.getPassportData().getNumber());
                 map.put("passport-issued", data.getPassportData().getIssuedBy());
@@ -121,13 +121,30 @@ public class GoogleDriveServiceImpl implements GoogleDriveService {
                 map.put("birthday", data.getInputBirthday());
                 map.put("email", data.getInputEmail());
                 map.put("phone-number", data.getInputPhoneNumber());
+                if (!setting.isOneTimePayment()) {
+                    map.put("onetime","4.2. Заказчик вправе в одностороннем внесудебном порядке отказаться от Договора. " +
+                            "Если Исполнитель до получения такого отказа приступил к исполнению Договора, " +
+                            "Исполнитель обязан возвратить Заказчику стоимость оплаченного, но не истёкшего периода тренинга, " +
+                            "за вычетом фактических расходов Исполнителя в размере 6 000 рублей в срок до 10 рабочих дней с даты расторжения Договора.");
+                    map.put("point","4.3. ");
+                } else {
+                    map.put("onetime","");
+                    map.put("point","4.2. ");
+                }
+                if (setting.isDiploma()) {
+                    map.put("diploma", "5.4. Исполнитель гарантирует, что выдаст Заказчику (Клиенту) диплом о профессиональной переподготовке по окончанию обучения.");
+                } else {
+                    map.put("diploma", "");
+                }
+                map.put("summa",setting.getPaymentAmount());
                 mlp.getMainDocumentPart().variableReplace(map);
                 Transliterator toLatinTrans = Transliterator.getInstance(CYRILLIC_TO_LATIN);
-                String fileName = toLatinTrans.transliterate(data.getInputName());
-                File file = new File(templatePathUrl + fileName + ".docx");
+                String fileName= toLatinTrans.transliterate(data.getInputLastName() + data.getInputFirstName());
+                File file = new File(TEMPLATE_PATH_URL + fileName + ".docx");
                 file.createNewFile();
                 Docx4J.save(mlp, file);
-                lastContractIdRepository.save(lastContractId);
+                projectProperties.setContractLastId(lastId);
+                projectPropertiesService.update(projectProperties);
                 return Optional.of(file);
             } catch (Exception e) {
                 logger.info("Error with load file");
--- src/main/java/com/ewp/crm/service/interfaces/GoogleDriveService.java
+++
@@ -1,9 +0,0 @@
-package com.ewp.crm.service.interfaces;
-
-import com.ewp.crm.models.ContractFormData;
-
-import java.util.Optional;
-
-public interface GoogleDriveService {
-    Optional<String> createContractWithData(ContractFormData data);
-}
