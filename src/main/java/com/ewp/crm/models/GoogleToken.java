7a3d51318dbdfb83006c11086d64b60d703c3c0b
yogo96 <yogoman96@mail.ru>
2019-03-23 01:43:53 +0300

    add refresh token

--- src/main/java/com/ewp/crm/models/GoogleToken.java
+++ src/main/java/com/ewp/crm/models/GoogleToken.java
@@ -11,24 +11,37 @@ public class GoogleToken {
     @Column(name = "id")
     private Long id;
 
-    private String value;
+    @Column(name = "access_token")
+    private String accessToken;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
 
     public GoogleToken() {
     }
 
-    public GoogleToken(String value) {
-        this.value = value;
+    public GoogleToken(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
     }
 
     public Long getId() {
         return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 }
