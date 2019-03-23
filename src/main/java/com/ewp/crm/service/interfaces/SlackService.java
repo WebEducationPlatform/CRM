daca707a5f9765533dd78344980ae85564799af4
Nikolay <mailto:miqolay@gmail.com>
2019-03-23 15:21:53 +0500

    remove slackProfile and connected features

--- src/main/java/com/ewp/crm/service/interfaces/SlackService.java
+++ src/main/java/com/ewp/crm/service/interfaces/SlackService.java
@@ -1,10 +1,5 @@
 package com.ewp.crm.service.interfaces;
 
-import com.ewp.crm.models.SlackProfile;
-
 public interface SlackService {
-    void memberJoinSlack(SlackProfile slackProfile);
-    SlackProfile receiveClientSlackProfileBySlackHashName(String slackHashName);
-
     String getEmailListFromJson(String json);
 }
--- src/main/java/com/ewp/crm/service/slack/SlackServiceImpl.java
+++ src/main/java/com/ewp/crm/service/slack/SlackServiceImpl.java
@@ -1,14 +1,7 @@
 package com.ewp.crm.service.slack;
 
-import com.ewp.crm.models.Client;
-import com.ewp.crm.models.ProjectProperties;
-import com.ewp.crm.models.SlackProfile;
-import com.ewp.crm.models.Status;
 import com.ewp.crm.service.impl.StudentServiceImpl;
 import com.ewp.crm.service.interfaces.*;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,13 +12,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Optional;
 
 @Service
 @PropertySources(value = {
@@ -70,32 +56,6 @@ public class SlackServiceImpl implements SlackService {
     }
 
     @Override
-    public SlackProfile receiveClientSlackProfileBySlackHashName(String slackHashName) {
-        try {
-            String url = "https://slack.com/api/users.info?token=" + LEGACY_TOKEN + "&user=" + slackHashName;
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            JsonNode actualObj = objectMapper.readTree(response.toString()).get("user").get("profile");
-            SlackProfile clientSlackProfile = objectMapper.treeToValue(actualObj, SlackProfile.class);
-            clientSlackProfile.setHashName(slackHashName);
-            return clientSlackProfile;
-        } catch (IOException | RuntimeException e) {
-            logger.warn("Can't receive Client Slack profile", e);
-        }
-        return new SlackProfile();
-    }
-
-    @Override
     public String getEmailListFromJson(String json) {
         try {
             StringBuilder result = new StringBuilder();
@@ -119,35 +79,5 @@ public class SlackServiceImpl implements SlackService {
             logger.warn("Can't parse emails from slack", e);
         }
         return "Error";
-    }
-
-    @Override
-    public void memberJoinSlack(SlackProfile slackProfile) {
-        Optional<Client> client = clientService.getClientByEmail(slackProfile.getEmail());
-        if (client.isPresent()) {
-            client.get().setSlackProfile(slackProfile);
-            slackProfile.setClient(client.get());
-            ProjectProperties projectProperties = propertiesService.get();
-            if (projectProperties == null || projectProperties.getDefaultStatusId() == null) {
-                logger.warn("Don't have projectProperties yet! Create it.");
-            } else {
-                if (statusService.get(projectProperties.getDefaultStatusId()).isPresent()) {
-                    Status newClientStatus = statusService.get(projectProperties.getDefaultStatusId()).get();
-                    client.get().setStatus(newClientStatus);
-                    newClientStatus.addClient(client.get());
-                    statusService.update(newClientStatus);
-                }
-            }
-            if (client.get().getStudent() == null) {
-                studentService.addStudentForClient(client.get());
-            }
-            clientHistoryService.createHistory("Slack, nickname: " + slackProfile.getDisplayName()
-                    + ". " + client.get().getName() + " " + client.get().getLastName() + " теперь студент").ifPresent(client.get()::addHistory);
-            clientService.updateClient(client.get());
-            logger.info("New member " + slackProfile.getDisplayName() + " "
-                    + slackProfile.getEmail() + " joined to general channel");
-        } else {
-            logger.info(slackProfile.getDisplayName() + " not a client joined to Slack general channel!");
-        }
     }
 }
