130e76b4996456797789c1d1d77c5bbb433abf87
Maksim Gusev <aquariusborelias@gmail.com>
2019-03-29 20:57:20 +0300

    modified reportService for new general report

--- src/main/java/com/ewp/crm/service/interfaces/ReportService.java
+++ src/main/java/com/ewp/crm/service/interfaces/ReportService.java
@@ -1,10 +1,5 @@
 package com.ewp.crm.service.interfaces;
 
-import java.util.Optional;
-
 public interface ReportService {
 
-    Optional<String> buildReport(String date);
-
-    Optional<String> buildReportOfLastMonth();
 }
