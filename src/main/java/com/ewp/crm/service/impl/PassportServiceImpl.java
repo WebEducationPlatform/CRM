f3acdfbb9ba3c6b2879192d5ea52ef0d35687110
yogo96 <yogoman96@mail.ru>
2019-03-31 01:26:59 +0300

    add crypt

--- src/main/java/com/ewp/crm/service/impl/PassportServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/PassportServiceImpl.java
@@ -1,48 +1,85 @@
 package com.ewp.crm.service.impl;
 
+import com.ewp.crm.configs.inteface.PassportConfig;
 import com.ewp.crm.models.Passport;
 import com.ewp.crm.repository.interfaces.PassportDAO;
 import com.ewp.crm.service.interfaces.PassportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Base64;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
 
 @Service
 public class PassportServiceImpl extends CommonServiceImpl<Passport> implements PassportService {
 
     private PassportDAO passportDAO;
+    private final byte[] decryptKey;
+    private static Logger logger = LoggerFactory.getLogger(PassportServiceImpl.class);
+
 
     @Autowired
-    public PassportServiceImpl(PassportDAO passportDAO) {
+    public PassportServiceImpl(PassportDAO passportDAO, PassportConfig passportConfig) {
         this.passportDAO = passportDAO;
+        decryptKey = passportConfig.getSecretKey();
     }
 
     @Override
     public Passport encode(Passport passport) {
-        passport.setSeries(encode(passport.getSeries()));
-        passport.setNumber(encode(passport.getSeries()));
-        passport.setIssuedBy(encode(passport.getIssuedBy()));
-        passport.setRegistration(encode(passport.getRegistration()));
+        passport.setSeries(encrypt(passport.getSeries()));
+        passport.setNumber(encrypt(passport.getNumber()));
+        passport.setIssuedBy(encrypt(passport.getIssuedBy()));
+        passport.setRegistration(encrypt(passport.getRegistration()));
         return passport;
     }
 
     @Override
     public Passport decode(Passport passport) {
-        passport.setSeries(decode(passport.getSeries()));
-        passport.setNumber(decode(passport.getSeries()));
-        passport.setIssuedBy(decode(passport.getIssuedBy()));
-        passport.setRegistration(decode(passport.getRegistration()));
+        passport.setSeries(decrypt(passport.getSeries()));
+        passport.setNumber(decrypt(passport.getSeries()));
+        passport.setIssuedBy(decrypt(passport.getIssuedBy()));
+        passport.setRegistration(decrypt(passport.getRegistration()));
         return passport;
     }
 
-    private String encode(String elem) {
-        return Base64.getEncoder().withoutPadding().encodeToString(elem.getBytes());
+    private String encrypt(String inputData) {
+        try {
+            SecretKey secretKey = new SecretKeySpec(decryptKey, 0, decryptKey.length, "AES");
+
+            Cipher aesCipher = Cipher.getInstance("AES");
+            aesCipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] byteCipherText = aesCipher.doFinal(inputData.getBytes());
+            return Arrays.toString(byteCipherText);
+        } catch (Exception e) {
+            logger.error("Error with encrypt passport data", e);
+        }
+        return null;
     }
 
-    private String decode(String elem) {
-        return new String(Base64.getDecoder().decode(elem));
+    private String decrypt(String inputData) {
+        try {
+            SecretKey secretKey = new SecretKeySpec(decryptKey, 0, decryptKey.length, "AES");
+
+            Cipher aesCipher = Cipher.getInstance("AES");
+            aesCipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] bytePlainText = aesCipher.doFinal(toByte(inputData));
+            return new String(bytePlainText);
+        } catch (Exception e) {
+            logger.error("Error with encrypt passport data", e);
+        }
+        return null;
+    }
+
+    private static byte[] toByte(String str) {
+        String[] byteValues = str.substring(1, str.length() - 1).split(",");
+        byte[] bytes = new byte[byteValues.length];
+
+        for (int i = 0, len = bytes.length; i < len; i++) {
+            bytes[i] = Byte.parseByte(byteValues[i].trim());
+        }
+        return bytes;
     }
 }
