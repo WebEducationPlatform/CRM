5dfdd9f607c2a5020b3835b19eaff7e9644667fc
Nikolay <mailto:miqolay@gmail.com>
2019-04-02 02:41:52 +0500

    post method add to controller

--- src/main/java/com/ewp/crm/controllers/rest/EmailExtraRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/EmailExtraRestController.java
@@ -1,5 +1,6 @@
 package com.ewp.crm.controllers.rest;
 
+import com.ewp.crm.models.Client;
 import com.ewp.crm.models.EmailExtra;
 import com.ewp.crm.service.interfaces.ClientService;
 import com.ewp.crm.service.interfaces.EmailExtraService;
@@ -9,16 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/rest/emailextra")
-@PreAuthorize("hasAnyAuthority('OWNER', 'ADMIN', 'USER')")
+//@PreAuthorize("hasAnyAuthority('OWNER', 'ADMIN', 'USER')")
 public class EmailExtraRestController {
 
     private static Logger logger = LoggerFactory.getLogger(EmailExtraRestController.class);
@@ -37,7 +35,7 @@ public class EmailExtraRestController {
     public ResponseEntity<List<EmailExtra>> getEmailExtrasByClient(@PathVariable Long clientId) {
         List<EmailExtra> emailExtras = emailExtraService.getAllEmailsExtraByClient(clientService.get(clientId));
         return ResponseEntity.ok(emailExtras);
-    } // /rest/emailextra/getByClient/2
+    } // http://localhost:9999/rest/emailextra/getByClient/2
 
     @GetMapping(value = "/delete/{emailExtraId}")
     public ResponseEntity delEmailExtras(@PathVariable Long emailExtraId) {
@@ -47,6 +45,19 @@ public class EmailExtraRestController {
             emailExtraService.delete(emailExtraId);
         }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    } // /rest/emailextra/delete/1
+    } //  http://localhost:9999/rest/emailextra/delete/1
 
+    @PostMapping(value = "/add")
+    public ResponseEntity<EmailExtra> addEmailExtra(@RequestParam(name = "clientId") Long clientId,
+                                              @RequestParam(name = "emailExtra") String mail) {
+        Client client = clientService.get(clientId);
+        if (client == null) {
+            logger.error("Can`t add additional email, client with id {} not found", clientId);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        EmailExtra emailExtra = new EmailExtra(mail, client);
+        emailExtraService.add(emailExtra);
+        return ResponseEntity.status(HttpStatus.CREATED).body(emailExtra);
+    } // http://localhost:9999/rest/emailextra/add [{"id":2,"emailExtra":"gribok@gmail.com"},{"id":3,"emailExtra":"elk@gmail.com"},{"id":4,"emailExtra":"parrot@gmail.com"}]
+      // {"id":5,"emailExtra":"elk23@gmail.com"}
 }
