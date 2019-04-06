0b55b436fa9cbed2254532ca86eaa64b11c42786
Maksim Gusev <aquariusborelias@gmail.com>
2019-04-06 13:58:23 +0300

    ReportService fixes

--- src/main/java/com/ewp/crm/service/interfaces/ReportService.java
+++ src/main/java/com/ewp/crm/service/interfaces/ReportService.java
@@ -3,10 +3,11 @@ package com.ewp.crm.service.interfaces;
 import com.ewp.crm.models.Status;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Set;
 
 public interface ReportService {
-    int countChangedStatusClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, Status from, Status to, Set<Status> exclude);
-    int countNewClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate);
+    int countChangedStatusClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, Long from, Long to, List<Long> excludeStatusesIds);
+    long countNewClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, List<Long> excludeStatusesIds);
     long countFirstPaymentClients(Status inProgressStatus, ZonedDateTime firstReportDate, ZonedDateTime lastReportDate);
 }
