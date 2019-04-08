f3aca816f196c1cd0efb62e407950fbde2936c33
Vladimir Yarovoy <shpi0@yandex.ru>
2019-04-08 14:28:59 +0300

    Fix reports

--- src/main/java/com/ewp/crm/service/impl/ReportServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/ReportServiceImpl.java
@@ -13,17 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Service
 public class ReportServiceImpl implements ReportService {
@@ -33,8 +25,6 @@ public class ReportServiceImpl implements ReportService {
     private final ClientRepository clientRepository;
     private final StatusService statusService;
     private final ProjectPropertiesService projectPropertiesService;
-
-    private long newClientStatus;
 
     @Autowired
     public ReportServiceImpl(ClientRepository clientRepository, StatusService statusService, ProjectPropertiesService projectPropertiesService) {
@@ -51,112 +41,132 @@ public class ReportServiceImpl implements ReportService {
      * @return количество найденных клиентов
      */
     @Override
-    public int countNewClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate) {
-        return clientRepository.getClientByHistoryTimeIntervalAndHistoryType(firstReportDate, lastReportDate,
-                new ClientHistory.Type[]{ClientHistory.Type.ADD, ClientHistory.Type.SOCIAL_REQUEST}).size();
+    public int countNewClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, List<Long> excludeStatusesIds) {
+        List<ClientHistory.Type> historyTypes = Arrays.asList(ClientHistory.Type.ADD, ClientHistory.Type.SOCIAL_REQUEST);
+        firstReportDate = ZonedDateTime.of(firstReportDate.toLocalDate().atStartOfDay(), ZoneId.systemDefault());
+        lastReportDate = ZonedDateTime.of(lastReportDate.toLocalDate().atTime(23, 59, 59), ZoneId.systemDefault());
+        List<Status> excludeStatuses = getAllStatusesByIds(excludeStatusesIds);
+        List<Client> clients = clientRepository.getClientByHistoryTimeIntervalAndHistoryType(firstReportDate, lastReportDate, historyTypes, excludeStatuses);
+        return clients.size();
     }
 
-    private ZonedDateTime[] parseTwoDate(String date) {
-        String date1 = date.substring(0, 10);
-        String date2 = date.substring(13, 23);
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        TemporalAccessor parse1 = format.parse(date1);
-        TemporalAccessor parse2 = format.parse(date2);
-        try {
-            ZonedDateTime localDate1 = LocalDate.from(parse1).atStartOfDay(ZoneId.systemDefault());
-            ZonedDateTime localDate2 = LocalDate.from(parse2).atStartOfDay(ZoneId.systemDefault()).plusHours(23).plusMinutes(59).plusSeconds(59);
-            return new ZonedDateTime[]{localDate1, localDate2};
-        } catch (DateTimeParseException e) {
-            logger.error("String \"" + date + "\" hasn't parsed");
-            return null;
+    private List<Status> getAllStatusesByIds(List<Long> ids) {
+        List<Status> result = new ArrayList<>();
+        if (ids != null) {
+            for (Long id : ids) {
+                statusService.get(id).ifPresent(result::add);
+            }
         }
+        return result;
     }
 
     /**
-     * Подсчитывает количество клиентов, которые перешли в статус to в заданный период firstReportDate - lastReportDate
-     * из статуса from и в данный момент не находятся в исключенных статусах exclude
+     * Получает название статуса, в который перевели клиента, из заголовка истории клиента
      *
-     * @param firstReportDate дата начала отчетного периода
-     * @param lastReportDate  дата окончания отчетного периода
-     * @param from            исходный статус клиента
-     * @param to              конечный статус клиента
-     * @param exclude         список исключенных статусов
+     * @param title
+     * @return название статуса
+     */
+    private Optional<String> parseStatusNameFromHistoryTitle(String title) {
+        String[] parse1 = title.split(": ");
+        if (parse1.length == 2) {
+            String[] parse2 = parse1[1].split(" из ");
+            if (parse2.length == 2) {
+                return Optional.of(parse2[0].trim());
+            } else {
+                return Optional.of(parse1[1].trim());
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Подсчитывает количество клиентов, которые перешли в статус toStatus в заданный период reportStartDate - reportEndDate
+     * из статуса fromStatus и в данный момент не находятся в исключенных статусах excludeStatuses
+     * Также проверяет, не являлся ли переход в статус ошибочным. В случае, если клиент вернулся в исходных статус в течение
+     * 24 часов с момента смены статуса, то считается, что смена статуса была ошибочной
+     *
+     * @param reportStartDate     дата начала отчетного периода
+     * @param reportEndDate       дата окончания отчетного периода
+     * @param fromStatusId        исходный статус клиента
+     * @param toStatusId          конечный статус клиента
+     * @param excludeStatusesIds  список исключенных статусов
      * @return количество подходящих под критерии клиентов
      */
     @Override
-    public int countChangedStatusClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, Status from, Status to, Set<Status> exclude) {
-        if (from == null || to == null || to.equals(from) || firstReportDate == null || lastReportDate == null) {
-            return -1;
-        }
-        if (lastReportDate.isBefore(firstReportDate) || ChronoUnit.DAYS.between(lastReportDate, lastReportDate) != 0) {
-            return -1;
-        }
-        // статус from для новых клиентов?
-        boolean isNewClient = false;
-        ProjectProperties pp = projectPropertiesService.getOrCreate();
-        newClientStatus = pp.getNewClientStatus();
-        if (newClientStatus == from.getId()) {
-            isNewClient = true;
-        }
+    public int countChangedStatusClients(ZonedDateTime reportStartDate, ZonedDateTime reportEndDate, long fromStatusId, long toStatusId, List<Long> excludeStatusesIds) {
+        Optional<Status> fromStatus = statusService.get(fromStatusId);
+        Optional<Status> toStatus = statusService.get(toStatusId);
+        List<Status> excludeStatuses = getAllStatusesByIds(excludeStatusesIds);
+        List<ClientHistory.Type> historyTypes = Collections.singletonList(ClientHistory.Type.STATUS);
         int result = 0;
-        // выбираем клиентов, которые на изменили стутус на заданный в выбранном периоде
-        List<Long> clientIds = clientRepository.getChangedStatusClientIdsInPeriod(firstReportDate, lastReportDate,
-                new ClientHistory.Type[]{ClientHistory.Type.STATUS}, to.getName());
-        List<Client> clients = clientRepository.getAllByIdIn(clientIds);
-        clients = clients.stream().filter(x -> !exclude.contains(x.getStatus())).collect(Collectors.toList());
-        for (Client client : clients) {
-            ListIterator li = client.getHistory().listIterator();
-            boolean needTo = true;
-            while (li.hasNext()) {
-                ClientHistory history = (ClientHistory) li.next();
-                // ищем первое вхождение с конца в истории клиента (to)
-                if (needTo && history.getType().equals(ClientHistory.Type.STATUS) && history.getTitle().contains(to.getName())) {
-                    needTo = false;
-                    continue;
-                }
-                // анализируем, является ли предыдущий статус - статусом нового клиента
-                if (isNewClient && !needTo && (history.getType().equals(ClientHistory.Type.ADD) || history.getType().equals(ClientHistory.Type.SOCIAL_REQUEST))) {
-                    result++;
-                    break;
-                }
-                // анализируем, является ли предыдущий статус клиента заданным (для старых клиентов)
-                if (!isNewClient && !needTo && history.getType().equals(ClientHistory.Type.STATUS)) {
-                    if (history.getTitle().contains(from.getName())) {
-                        result++;
+        if (fromStatus.isPresent() && toStatus.isPresent() && !toStatus.equals(fromStatus) && reportStartDate != null && reportEndDate != null) {
+            reportStartDate = ZonedDateTime.of(reportStartDate.toLocalDate().atStartOfDay(), ZoneId.systemDefault());
+            reportEndDate = ZonedDateTime.of(reportEndDate.toLocalDate().atTime(23, 59, 59), ZoneId.systemDefault());
+            // статус fromStatus для новых клиентов?
+            ProjectProperties pp = projectPropertiesService.getOrCreate();
+            long newClientStatus = pp.getNewClientStatus();
+            boolean isNewClient = newClientStatus == fromStatus.get().getId();
+            // Выбираем клиентов, которые изменили статус на заданный в выбранном периоде
+            List<Client> clients = clientRepository.getChangedStatusClientsInPeriod(reportStartDate, reportEndDate, historyTypes, excludeStatuses, toStatus.get().getName());
+            for (Client client : clients) {
+                // Получаем запись истории клиента по тем же параметрам, по которым выше был отобран клиент
+                ClientHistory clientHistory = clientRepository.getHistoryByClientAndHistoryTimeIntervalAndHistoryType(client, reportStartDate, reportEndDate, historyTypes, toStatus.get().getName());
+                if (clientHistory != null) {
+                    // Получаем записи из истории клиента, предшествующую и последующую записи выше
+                    ClientHistory beforeHistory = clientRepository.getNearestClientHistoryBeforeDate(client, clientHistory.getDate(), historyTypes);
+                    ClientHistory afterHistory = clientRepository.getNearestClientHistoryAfterDate(client, clientHistory.getDate(), historyTypes);
+                    // Если записи с переходами в истории есть, получаем из них статусы -
+                    // в каком статусе был клиент ранее и в какой статус перешел после.
+                    // Если статусы совпадают, проверяем дату возврата в исходный статус.
+                    // Если возврат в исходный статус произошел в течение 24 часов, то это считается ошибочным переносом
+                    // и исключается из результатов отчета.
+                    if (beforeHistory != null) {
+                        Optional<String> beforeStatus = parseStatusNameFromHistoryTitle(beforeHistory.getTitle());
+                        if (beforeStatus.isPresent() && beforeStatus.get().equals(fromStatus.get().getName())) {
+                            result++;
+                            if (afterHistory != null) {
+                                Optional<String> afterStatus = parseStatusNameFromHistoryTitle(afterHistory.getTitle());
+                                if (afterStatus.isPresent() && beforeStatus.get().equals(afterStatus.get()) && clientHistory.getDate().plusDays(1L).isAfter(afterHistory.getDate())) {
+                                    result--;
+                                }
+                            }
+                        }
+                    } else {
+                        // Если ищем переходы из статуса для Новых клиентов, то удостоверяемся, что ранее переходов в другие статусы не было
+                        if (isNewClient) {
+                            result++;
+                        }
                     }
-                    break;
                 }
             }
         }
         return result;
     }
 
-
     /**
      * Подсчитывает количество студентов, которые впервые совершили оплату в заданный период
      *
-     * @param inProgressStatus статус, который получает клиент при оплате
-     * @param firstReportDate  дата начала отчетного периода
-     * @param lastReportDate   дата окончания отчетного периода
+     * @param reportStartDate  дата начала отчетного периода
+     * @param reportEndDate   дата окончания отчетного периода
      * @return количество студентов, впервые совершивших оплату в заданный период
      */
     @Override
-    public long countFirstPaymentClients(Status inProgressStatus, ZonedDateTime firstReportDate, ZonedDateTime lastReportDate) {
+    public long countFirstPaymentClients(ZonedDateTime reportStartDate, ZonedDateTime reportEndDate, List<Long> excludeStatusesIds) {
         long result = 0;
-        List<Client> allClients = clientRepository.findAll();
-        for (Client client : allClients) {
-            // поиск записи о первой оплате клиента
-            ClientHistory clientFirstPayment = client.getHistory().stream()
-                    .filter(x -> x.getTitle().contains(inProgressStatus.getName()))
-                    .findFirst()
-                    .orElse(null);
-            if (clientFirstPayment == null) {
-                break;
-            }
-            // проверка попадания оплаты в заданный период
-            ZonedDateTime firstPaymentDate = clientFirstPayment.getDate();
-            if (firstPaymentDate.isAfter(firstReportDate) && firstPaymentDate.isBefore(lastReportDate)) {
-                result++;
+        // Получаем статус, в который переходит клиент после первой оплаты
+        Optional<Status> inProgressStatus = statusService.get(4L);
+        if (inProgressStatus.isPresent()) {
+            List<ClientHistory.Type> historyTypes = Collections.singletonList(ClientHistory.Type.STATUS);
+            List<Status> excludeStatuses = getAllStatusesByIds(excludeStatusesIds);
+            reportStartDate = ZonedDateTime.of(reportStartDate.toLocalDate().atStartOfDay(), ZoneId.systemDefault());
+            reportEndDate = ZonedDateTime.of(reportEndDate.toLocalDate().atTime(23, 59, 59), ZoneId.systemDefault());
+            // Получение всех клиентов, которые перешли в статус в заданный период
+            List<Client> clients = clientRepository.getChangedStatusClientsInPeriod(reportStartDate, reportEndDate, historyTypes, excludeStatuses, inProgressStatus.get().getName());
+            // Для каждого клиента проверяем, впервые ли ему присвоен данный статус
+            for (Client client :clients) {
+                if (!clientRepository.hasClientBeenInStatusBefore(client.getId(), reportStartDate, inProgressStatus.get().getName())) {
+                    result++;
+                }
             }
         }
         return result;
