27cf8b3d4d1999292b01c5519b63789f70baa3ba
Vladimir Yarovoy <shpi0@yandex.ru>
2019-04-14 19:52:01 +0300

    Unused code removing

--- src/main/java/com/ewp/crm/service/google/GoogleCalendarServiceImpl.java
+++ src/main/java/com/ewp/crm/service/google/GoogleCalendarServiceImpl.java
@@ -3,18 +3,9 @@ package com.ewp.crm.service.google;
 import com.ewp.crm.configs.GoogleAPIConfigImpl;
 import com.ewp.crm.models.Client;
 import com.ewp.crm.models.GoogleToken;
-import com.ewp.crm.service.interfaces.GoogleAuthorizationService;
 import com.ewp.crm.service.interfaces.GoogleCalendarService;
 import com.ewp.crm.service.interfaces.GoogleTokenService;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.EventDateTime;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.ibm.icu.text.Transliterator;
@@ -39,12 +30,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.time.Instant;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -52,22 +39,14 @@ public class GoogleCalendarServiceImpl implements GoogleCalendarService {
 
     private final static String CYRILLIC_TO_LATIN = "Russian-Latin/BGN";
 	private static Logger logger = LoggerFactory.getLogger(GoogleCalendarServiceImpl.class);
-	private Calendar calendarBuilder;
-	private final String APPLICATION_NAME = "client-app";
-	private HttpTransport httpTransport;
-	private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-	private Credential credential;
-
-	private final GoogleAuthorizationService authorizationService;
 	private final GoogleTokenService tokenService;
 	private final String calendarName;
 	private final String eventName;
 
 	@Autowired
-	public GoogleCalendarServiceImpl(GoogleAuthorizationService authorizationService, GoogleTokenService tokenService,
+	public GoogleCalendarServiceImpl(GoogleTokenService tokenService,
                                      GoogleAPIConfigImpl config) {
-		this.authorizationService = authorizationService;
 		this.tokenService = tokenService;
 		this.calendarName = config.getCalendarName();
 		this.eventName = config.getEventName();
@@ -228,125 +207,4 @@ public class GoogleCalendarServiceImpl implements GoogleCalendarService {
             }
         }
     }
-
-	@Override
-	public Calendar calendarBuilder() {
-        try {
-            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-            calendarBuilder = new Calendar.Builder(httpTransport, JSON_FACTORY, credential)
-                    .setApplicationName(APPLICATION_NAME).build();
-        } catch (GeneralSecurityException | IOException e) {
-            logger.error("Error to build calendar ", e);
-        }
-        return calendarBuilder;
-	}
-
-	@Override
-	public Calendar getCalendarBuilder() {
-		return calendarBuilder;
-	}
-
-	@Override
-	public boolean googleAuthorizationIsNotNull(){
-		return calendarBuilder != null;
-	}
-
-	@Override
-	public boolean checkFreeDateAndCorrectEmail(Long date, String calendarMentor) {
-		try {
-			com.google.api.services.calendar.model.Calendar calendar = calendarBuilder.calendars().get(calendarMentor).execute();
-			List<Event> eventAll = calendarBuilder.events().list(calendar.getId()).execute().getItems();
-			String format = getFormatZonedDateTime(date, calendar);
-			for (Event anEventAll : eventAll) {
-				if (anEventAll.getStart().toString().contains(format)) {
-					return true;
-				}
-			}
-		} catch (IOException e) {
-			logger.error("Error to send message ", e);
-		}
-		return false;
-	}
-
-	private void checkTokenAndAuthorize() {
-        if (!googleAuthorizationIsNotNull()) {
-            calendarBuilder();
-        }
-        Optional<GoogleToken> refreshedToken = tokenService.getRefreshedToken(GoogleTokenService.TokenType.CALENDAR);
-        refreshedToken.ifPresent(googleToken -> credential.setRefreshToken(googleToken.getRefreshToken()));
-    }
-
-	@Override
-	public void addEvent(String calendarMentor, Long startDate, Client client) throws IOException {
-        checkTokenAndAuthorize();
-        if (googleAuthorizationIsNotNull()) {
-            Event event = newEvent(startDate, client);
-            com.google.api.services.calendar.model.Calendar calendar =
-                    calendarBuilder.calendars().get(calendarMentor).execute();
-            calendarBuilder.events().insert(calendar.getId(), event).execute();
-        }
-	}
-
-	@Override
-	public void update(Long newDate, Long oldDate, String calendarMentor, Client client) throws IOException {
-        checkTokenAndAuthorize();
-        if (googleAuthorizationIsNotNull()) {
-            com.google.api.services.calendar.model.Calendar calendar =
-                    calendarBuilder.calendars().get(calendarMentor).execute();
-            String formattedDateOld = getFormatZonedDateTime(oldDate, calendar);
-            Event newEvent = newEvent(newDate, client);
-            List<Event> eventAll = calendarBuilder.events().list(calendar.getId()).execute().getItems();
-            for (int i = 0; i < eventAll.size(); i++) {
-                if (eventAll.get(i).getStart().toString().contains(formattedDateOld)) {
-                    String eventId = eventAll.get(i).getId();
-                    calendarBuilder.events().delete(calendarMentor, eventId).execute();
-                    calendarBuilder.events().insert(calendar.getId(), newEvent).execute();
-                }
-            }
-        }
-	}
-
-	@Override
-	public void delete(Long oldDate, String calendarMentor) {
-        checkTokenAndAuthorize();
-        if (googleAuthorizationIsNotNull()) {
-            try {
-                com.google.api.services.calendar.model.Calendar calendar =
-                        calendarBuilder.calendars().get(calendarMentor).execute();
-                String format = getFormatZonedDateTime(oldDate, calendar);
-                List<Event> eventAll = calendarBuilder.events().list(calendar.getId()).execute().getItems();
-                for (int i = 0; i < eventAll.size(); i++) {
-                    if (eventAll.get(i).getStart().toString().contains(format)) {
-                        String eventId = eventAll.get(i).getId();
-                        calendarBuilder.events().delete(calendarMentor, eventId).execute();
-                    }
-                }
-            } catch (IOException e) {
-                logger.error("Error to send message ", e);
-            }
-        }
-	}
-
-	private Event newEvent(Long startDate, Client client) {
-		Event event = new Event();
-		event.setSummary("CRM - " + client.getName() + " " + client.getLastName() + " (Skype: " + client.getSkype() + ")");
-		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-		String format = Instant.ofEpochMilli(startDate)
-				.atZone(ZoneId.of("+00:00"))
-				.withZoneSameLocal(ZoneId.of("Europe/Moscow"))
-				.format(outputFormatter);
-		DateTime start = new DateTime(format);
-		event.setStart(new EventDateTime().setDateTime(start));
-		DateTime end = new DateTime(format);
-		event.setEnd(new EventDateTime().setDateTime(end));
-		return event;
-	}
-
-	private String getFormatZonedDateTime(Long date, com.google.api.services.calendar.model.Calendar calendar) {
-		return Instant.ofEpochMilli(date)
-				.atZone(ZoneId.of("+00:00"))
-				.withZoneSameLocal(ZoneId.of("Europe/Moscow"))
-				.withZoneSameInstant(ZoneId.of(calendar.getTimeZone()))
-				.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
-	}
 }
--- src/main/java/com/ewp/crm/service/interfaces/GoogleCalendarService.java
+++ src/main/java/com/ewp/crm/service/interfaces/GoogleCalendarService.java
@@ -1,9 +1,7 @@
 package com.ewp.crm.service.interfaces;
 
 import com.ewp.crm.models.Client;
-import com.google.api.services.calendar.Calendar;
 
-import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
@@ -14,19 +12,5 @@ public interface GoogleCalendarService {
     void updateCalendarEvent(String eventId, ZonedDateTime eventStart, Client client);
 
     void deleteCalendarEvent(String eventId);
-
-    Calendar calendarBuilder();
-
-    Calendar getCalendarBuilder();
-
-    boolean googleAuthorizationIsNotNull();
-
-    void addEvent(String calendarMentor, Long startDate, Client skype) throws IOException;
-
-    void update(Long newDate, Long oldDate, String calendarMentor, Client skype) throws IOException;
-
-    boolean checkFreeDateAndCorrectEmail(Long newDate, String calendarMentor);
-
-    void delete(Long oldDate, String calendarMentor);
 
 }
