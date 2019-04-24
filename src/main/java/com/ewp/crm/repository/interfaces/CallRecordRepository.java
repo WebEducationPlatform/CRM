99ba09fbe577417a5a10fb80ecfcdff5f4275dc8
yogo96 <yogoman96@mail.ru>
2019-04-24 22:36:44 +0300

    add sorting methods

--- src/main/java/com/ewp/crm/repository/interfaces/CallRecordRepository.java
+++ src/main/java/com/ewp/crm/repository/interfaces/CallRecordRepository.java
@@ -1,10 +1,12 @@
 package com.ewp.crm.repository.interfaces;
 
 import com.ewp.crm.models.CallRecord;
-import org.drinkless.tdlib.TdApi;
+import com.ewp.crm.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Pageable;
+
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Repository
@@ -13,6 +15,7 @@ public interface CallRecordRepository extends CommonGenericRepository<CallRecord
     @Query("FROM CallRecord callRecord ORDER BY callRecord.id DESC")
     List<CallRecord> getAllCommonRecords(Pageable pageable);
 
-    List<CallRecord> findAllByDateBetweenAAndCallingUser();
+    List<CallRecord> findAllByCallingUserAndDateBetweenOrderByDateDesc(User user, ZonedDateTime from, ZonedDateTime to, Pageable pageable);
 
+    List<CallRecord> findAllByDateBetweenOrderByDateDesc(ZonedDateTime from, ZonedDateTime to, Pageable pageable);
 }
