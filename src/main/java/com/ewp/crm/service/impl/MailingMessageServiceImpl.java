db95ff7cd08ceb36388c655c11c7c6ee0c98e8df
yogo96 <yogoman96@mail.ru>
2019-05-10 15:16:28 +0300

    change methods

--- src/main/java/com/ewp/crm/service/impl/MailingMessageServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/MailingMessageServiceImpl.java
@@ -1,14 +1,13 @@
 package com.ewp.crm.service.impl;
 
-
 import com.ewp.crm.models.ClientData;
 import com.ewp.crm.models.MailingMessage;
 import com.ewp.crm.repository.interfaces.MailingMessageRepository;
 import com.ewp.crm.service.interfaces.MailingMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -21,20 +20,19 @@ public class MailingMessageServiceImpl extends CommonServiceImpl<MailingMessage>
         this.mailingMessageRepository = mailingMessageRepository;
     }
 
-
     @Override
-    public List<MailingMessage> getUserMail(long userId) {
-        return mailingMessageRepository.getUserMail(userId);
+    public List<MailingMessage> getMailingMessageByUserId(Long userId) {
+        return mailingMessageRepository.findAllByUserId(userId);
     }
 
     @Override
-    public List<MailingMessage> getUserByIdAndDate(long userId, int timeFrom, int timeTo) {
-        return mailingMessageRepository.getUserByIdAndDate(userId, timeFrom, timeTo);
+    public List<MailingMessage> getMailingMessageByUserIdAndDate(Long userId, LocalDateTime from, LocalDateTime to) {
+        return mailingMessageRepository.findAllByUserIdAndDateBetweenOrderByDateDesc(userId, from, to);
     }
 
     @Override
-    public List<MailingMessage> getUserByDate(int timeFrom, int timeTo) {
-        return mailingMessageRepository.getUserByDate(timeFrom, timeTo);
+    public List<MailingMessage> getMailingMessageByDate(LocalDateTime from, LocalDateTime to) {
+        return mailingMessageRepository.findAllByDateBetweenOrderByDateDesc(from, to);
     }
 
     public List<ClientData> getClientDataById(Long id) {
