25658530c49d4801ab26c98973f8015f335ea32a
SanKur <kurmasasha@gmail.com>
2019-05-12 13:15:11 +0300

    Fix report for new user

--- src/main/java/com/ewp/crm/service/interfaces/ReportService.java
+++ src/main/java/com/ewp/crm/service/interfaces/ReportService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface ReportService {
     int countChangedStatusClients(ZonedDateTime reportStartDate, ZonedDateTime reportEndDate, long fromStatusId, long toStatusId, List<Long> excludeStatusesIds);
 
-    int countNewClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, List<Long> excludeStatusesIds);
+    String countNewClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, List<Long> excludeStatusesIds);
 
     long countFirstPaymentClients(ZonedDateTime firstReportDate, ZonedDateTime lastReportDate, List<Long> excludeStatusesIds);
 
