ee220a299a0a485c73e058499fd523393b79b860
fpyrypt <fpyrypt@gmail.com>
2019-05-18 18:58:09 +0300

    Refactored client card loading.

--- src/main/java/com/ewp/crm/controllers/rest/ClientRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/ClientRestController.java
@@ -2,6 +2,7 @@ package com.ewp.crm.controllers.rest;
 
 import com.ewp.crm.models.*;
 import com.ewp.crm.models.SortedStatuses.SortingType;
+import com.ewp.crm.models.dto.ClientCardDtoBuilder;
 import com.ewp.crm.repository.interfaces.ClientRepository;
 import com.ewp.crm.service.interfaces.*;
 import org.slf4j.Logger;
@@ -499,4 +500,9 @@ public class ClientRestController {
 		return student;
 	}
 
+	@GetMapping("/card/{id}")
+	@PreAuthorize("hasAnyAuthority('OWNER', 'ADMIN', 'USER','MENTOR')")
+	public ResponseEntity<String> getClientCardDto(@PathVariable Long id) {
+		return ResponseEntity.ok(ClientCardDtoBuilder.buildClientCardDto(clientService.get(id), statusService.getAll()));
+	}
 }
