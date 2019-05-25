45e73ac924236fb045ab9ede86991e7ffa5eb94f
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-05-25 19:07:03 +0900

    Merge branch 'dev' into javalearn
    # Conflicts:
    #	src/main/java/com/ewp/crm/configs/GoogleEmailConfig.java

--- src/main/java/com/ewp/crm/service/impl/ClientServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/ClientServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.ZonedDateTime;
 import java.util.*;
 
-
 @Service
 public class ClientServiceImpl extends CommonServiceImpl<Client> implements ClientService {
 
