4dc6b116741e20ffe5c6477da34e9d9cf4ff2c32
yaegorko <yaegorko@gmail.com>
2019-06-13 00:18:20 +0400

    start mentors page

--- src/main/java/com/ewp/crm/repository/interfaces/StudentRepository.java
+++ src/main/java/com/ewp/crm/repository/interfaces/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.ewp.crm.repository.interfaces;
 
 import com.ewp.crm.models.Student;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface StudentRepository extends CommonGenericRepository<Student> {
     Student getStudentByClientId(Long clientId);
 
     List<Student> getStudentsByClientSocialProfiles_Empty();
+
+    @Query("SELECT s FROM Student s JOIN s.client c WHERE ?1 MEMBER OF c.clientEmails")
+    Student getStudentByEmail(String email);
 }
