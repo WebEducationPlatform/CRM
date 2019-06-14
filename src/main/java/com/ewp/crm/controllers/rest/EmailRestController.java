61f5946499de82f361e73b0c86a2c35b492ad767
yaegorko <yaegorko@gmail.com>
2019-06-14 20:34:59 +0400

    mentors v1.1

--- src/main/java/com/ewp/crm/controllers/rest/EmailRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/EmailRestController.java
@@ -9,8 +9,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
-
 @RestController
 public class EmailRestController {
 
