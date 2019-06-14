aaa51c8b8976fb940e83da2c72f828317e2aa2d4
Vladimir Yarovoy <shpi0@yandex.ru>
2019-06-14 10:44:47 +0300

    Adding clients with non-VK social networks fix

--- src/main/java/com/ewp/crm/service/interfaces/FacebookService.java
+++ src/main/java/com/ewp/crm/service/interfaces/FacebookService.java
@@ -2,8 +2,12 @@ package com.ewp.crm.service.interfaces;
 
 import com.ewp.crm.exceptions.util.FBAccessTokenException;
 
+import java.util.Optional;
+
 public interface FacebookService {
 
 	void getFacebookMessages() throws FBAccessTokenException;
+
+	Optional<String> getIdFromLink(String link);
 }
 
