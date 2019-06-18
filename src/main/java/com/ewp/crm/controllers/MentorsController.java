ccddd1b4a77e19edccfbd6a1a57e8f1a37a71cca
Egor Kuznetsov <yaegorko@gmail.com>
2019-06-18 12:14:24 +0400

    mentors v2.1

--- src/main/java/com/ewp/crm/controllers/MentorsController.java
+++ src/main/java/com/ewp/crm/controllers/MentorsController.java
@@ -7,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,55 +21,48 @@ import java.util.stream.Collectors;
 @PropertySource("file:./slackbot.properties")
 public class MentorsController {
 
-    private static Logger logger = LoggerFactory.getLogger(HrController.class);
+	private static Logger logger = LoggerFactory.getLogger(HrController.class);
 
-    private final StatusService statusService;
-    private final ClientService clientService;
-    private final UserService userService;
-    private final MessageTemplateService messageTemplateService;
-    private final ProjectPropertiesService propertiesService;
-    private final StudentStatusService studentStatus;
-    private final RoleService roleService;
-    private final StudentService studentService;
+	private final StatusService statusService;
+	private final UserService userService;
+	private final MessageTemplateService messageTemplateService;
+	private final ProjectPropertiesService propertiesService;
+	private final StudentStatusService studentStatus;
+	private final RoleService roleService;
 
 
-    @Autowired
-    public MentorsController(StatusService statusService,
-                             ClientService clientService,
-                             UserService userService,
-                             MessageTemplateService MessageTemplateService,
-                             ProjectPropertiesService propertiesService,
-                             StudentStatusService studentStatus,
-                             RoleService roleService,
-							 StudentService studentService) {
-        this.statusService = statusService;
-        this.clientService = clientService;
-        this.userService = userService;
-        this.messageTemplateService = MessageTemplateService;
-        this.propertiesService = propertiesService;
-        this.studentStatus = studentStatus;
-        this.roleService = roleService;
-        this.studentService = studentService;
-    }
+	@Autowired
+	public MentorsController(StatusService statusService,
+							 UserService userService,
+							 MessageTemplateService MessageTemplateService,
+							 ProjectPropertiesService propertiesService,
+							 StudentStatusService studentStatus,
+							 RoleService roleService) {
+		this.statusService = statusService;
+		this.userService = userService;
+		this.messageTemplateService = MessageTemplateService;
+		this.propertiesService = propertiesService;
+		this.studentStatus = studentStatus;
+		this.roleService = roleService;
+	}
 
-    @Value("${slackbot.ip}")
-    private String slackBotIp;
-    @Value("${slackbot.port}")
-    private String slackBotPort;
+	@Value("${slackbot.ip}")
+	private String slackBotIp;
+	@Value("${slackbot.port}")
+	private String slackBotPort;
 
-    @GetMapping
-    public ModelAndView showMentorsWithThearStudents() {
-        ModelAndView modelAndView = new ModelAndView("mentors-with-students-table");
-        modelAndView.addObject("slackBotIp", slackBotIp);
-        modelAndView.addObject("slackBotPort", slackBotPort);
-        modelAndView.addObject("mentors", userService.getAll().stream().filter(x -> x.getRole().contains(roleService.getRoleByName("MENTOR"))).collect(Collectors.toList()));
-        //modelAndView.addObject("allClients", clientService.getAllClientsByPage(PageRequest.of(0, 15, Sort.by(Sort.Direction.DESC, "dateOfRegistration"))));
-        modelAndView.addObject("studentStatuses", studentStatus.getAll());
-        modelAndView.addObject("statuses", statusService.getAll());
-        modelAndView.addObject("projectProperties", propertiesService.get());
-        modelAndView.addObject("users", userService.getAll());
-        modelAndView.addObject("socialNetworkTypes", new SocialProfile().getAllSocialNetworkTypes());
-        modelAndView.addObject("emailTmpl", messageTemplateService.getAll());
-        return modelAndView;
-    }
+	@GetMapping
+	public ModelAndView showMentorsWithThearStudents() {
+		ModelAndView modelAndView = new ModelAndView("mentors-with-students-table");
+		modelAndView.addObject("slackBotIp", slackBotIp);
+		modelAndView.addObject("slackBotPort", slackBotPort);
+		modelAndView.addObject("mentors", userService.getAll().stream().filter(x -> x.getRole().contains(roleService.getRoleByName("MENTOR"))).collect(Collectors.toList()));
+		modelAndView.addObject("studentStatuses", studentStatus.getAll());
+		modelAndView.addObject("statuses", statusService.getAll());
+		modelAndView.addObject("projectProperties", propertiesService.get());
+		modelAndView.addObject("users", userService.getAll());
+		modelAndView.addObject("socialNetworkTypes", new SocialProfile().getAllSocialNetworkTypes());
+		modelAndView.addObject("emailTmpl", messageTemplateService.getAll());
+		return modelAndView;
+	}
 }
