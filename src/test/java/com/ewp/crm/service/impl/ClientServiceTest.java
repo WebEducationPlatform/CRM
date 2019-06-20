9178631aa5662365823940ad99fb2965a428b275
Nikolay <mailto:miqolay@gmail.com>
2019-06-21 00:03:17 +0500

    add comments to test methods

--- src/test/java/com/ewp/crm/service/impl/ClientServiceTest.java
+++ src/test/java/com/ewp/crm/service/impl/ClientServiceTest.java
@@ -11,9 +11,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-/*
-Тестирование ClientService на отдельной базе данных
-Перед запуском теста не забудьте создать базу данных crmtest, на которой будет происходить тестирование,
+/**
+Тестирование CRUD операций ClientService на отдельной базе данных
+Перед запуском теста создать базу данных crmtest, на которой будет происходить тестирование,
 настройки хранятся в файле test\resources\application-test.properties,
 если убрать (properties = "spring.profiles.active=test") - тестирование произодет на реальной базе проекта
  */
@@ -27,6 +27,7 @@ public class ClientServiceTest {
     @Autowired
     private ClientService clientService;
 
+    //проверяет, равен ли созданный клиент клиенту, добавленному в базу методом add() клиентсервиса
     @Test
     public void testAdd() {
         String expectedName = "Test_clientService_add";
@@ -35,17 +36,20 @@ public class ClientServiceTest {
         Assert.assertEquals(expectedClient, actualClient);
     }
 
+    //проверяет, равен ли созданный клиент клиенту, прочитанному из базы методом getClientByID() клиентсервиса
     @Test
     public void testGet() {
         String expectedName = "Test_clientService_get";
         Client expectedClient = new Client.Builder(expectedName).build();
         clientService.add(expectedClient);
         Long id = expectedClient.getId();
+        //проверяет, добавлен ли клиент в базу данных (id != null) перед чтением из базы
         Assert.assertNotNull(id);
         Client actualClient = clientService.getClientByID(id).isPresent() ? clientService.getClientByID(id).get() : null;
         Assert.assertEquals(expectedClient, actualClient);
     }
 
+    //проверяет, равен ли измененный клиент клиенту, прочитанному из базы после работы метода updateClient() клиентсервиса
     @Test
     public void testUpdate() {
         String oldName = "Test_clientService_update";
@@ -54,17 +58,20 @@ public class ClientServiceTest {
         String expectedName = "Test_clientService_updated";
         expectedClient.setName(expectedName);
         Long id = expectedClient.getId();
+        //проверяет, добавлен ли клиент в базу данных (id != null) перед обновлением
         Assert.assertNotNull(id);
         clientService.updateClient(expectedClient);
         Client actualClient = clientService.get(id);
         Assert.assertEquals(expectedClient, actualClient);
     }
 
+    //проверяет, равен ли удаленный клиент null после работы метода delete() клиентсервиса
     @Test
     public void testDelete() {
         Client client = new Client.Builder("Test_clientService_delete").build();
         clientService.add(client);
         Long id = client.getId();
+        //проверяет, добавлен ли клиент в базу данных (id != null) перед удалением
         Assert.assertNotNull(id);
         clientService.delete(id);
         Client deleted = clientService.get(id);
