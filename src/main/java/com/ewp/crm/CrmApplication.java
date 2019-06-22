bf6927a6c846a906dfc6b250b24b70d5895d6e27
Sergey Kubatko <skubatko@gmail.com>
2019-06-22 20:50:35 +0300

    feature/hr-statistics: CrmApplication  rolled back

--- src/main/java/com/ewp/crm/CrmApplication.java
+++ src/main/java/com/ewp/crm/CrmApplication.java
@@ -1,6 +1,5 @@
 package com.ewp.crm;
 
-import com.ewp.crm.configs.initializer.DataInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,8 +11,8 @@ public class CrmApplication {
 	}
 
 //	@Bean(initMethod = "init")
-	public DataInitializer initTestData() {
-		return new DataInitializer();
-	}
+//	public DataInitializer initTestData() {
+//		return new DataInitializer();
+//	}
 
 }
