d465c9db51d4199581bc2c931e9b7e4f9e6438c3
Vladimir Yarovoy <shpi0@yandex.ru>
2019-07-10 08:57:16 +0300

    New reports generating methods

--- src/main/java/com/ewp/crm/repository/interfaces/ClientStatusChangingHistoryRepositoryCustom.java
+++ src/main/java/com/ewp/crm/repository/interfaces/ClientStatusChangingHistoryRepositoryCustom.java
@@ -1,4 +1,21 @@
 package com.ewp.crm.repository.interfaces;
 
+import com.ewp.crm.models.Client;
+import com.ewp.crm.models.Status;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
 public interface ClientStatusChangingHistoryRepositoryCustom {
+
+    List<Client> getClientsEverBeenInStatusButExcludeStatuses(Status status, Status... excludeStatuses);
+
+    List<Client> getClientsBeenInStatusAtPeriodButExcludeStatuses(Status status, ZonedDateTime beginDate, ZonedDateTime endDate, Status... excludeStatuses);
+
+    void markAllFakeStatusesByChangingInIntervalRule(int minutes);
+
+    void markAllFakeStatusesByReturningInIntervalRule(int hours);
+
+    List<Client> getClientsWhoChangedStatusInPeriodButExcludeStatuses(Status sourceStatus, Status destinationStatus, ZonedDateTime beginDate, ZonedDateTime endDate, Status... excludeStatuses);
+
 }
--- src/main/java/com/ewp/crm/service/interfaces/ClientStatusChangingHistoryService.java
+++ src/main/java/com/ewp/crm/service/interfaces/ClientStatusChangingHistoryService.java
@@ -2,7 +2,9 @@ package com.ewp.crm.service.interfaces;
 
 import com.ewp.crm.models.Client;
 import com.ewp.crm.models.ClientStatusChangingHistory;
+import com.ewp.crm.models.Status;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +17,15 @@ public interface ClientStatusChangingHistoryService extends CommonService<Client
     Optional<ClientStatusChangingHistory> getFirstClientStatusChangingHistoryByClient(Client client);
 
     Optional<ClientStatusChangingHistory> getLastClientStatusChangingHistoryByClient(Client client);
+
+    List<Client> getClientsEverBeenInStatusButExcludeStatuses(Status status, Status... excludeStatuses);
+
+    List<Client> getClientsBeenInStatusAtPeriodButExcludeStatuses(Status status, ZonedDateTime beginDate, ZonedDateTime endDate, Status... excludeStatuses);
+
+    List<Client> getClientsWhoChangedStatusInPeriodButExcludeStatuses(Status sourceStatus, Status destinationStatus, ZonedDateTime beginDate, ZonedDateTime endDate, Status... excludeStatuses);
+
+    void markAllFakeStatusesByChangingInIntervalRule(int minutes);
+
+    void markAllFakeStatusesByReturningInIntervalRule(int hours);
 
 }
