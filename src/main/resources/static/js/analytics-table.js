5624a9e1973f8758c2a01cce4d266ac15901e8f9
Sergey Kubatko <skubatko@gmail.com>
2019-07-20 14:34:21 +0300

    feature/analytics: datepicker + chart preliminary setup

---
+++ src/main/resources/static/js/analytics-table.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    $.datepicker.setDefaults($.datepicker.regional["ru"]);
+    showAnalyticsChart();
+});
+
+$(function () {
+    $("#date-from-picker").datepicker();
+    $("#date-to-picker").datepicker();
+});
+
+function showAnalyticsChart() {
+
+    const ctx = document.getElementById('analytics-chart').getContext('2d');
+    const config = {
+        type: 'line',
+        data: {
+            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            datasets: [{
+                data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                label: "Africa",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+                label: "Asia",
+                borderColor: "#8e5ea2",
+                fill: false
+            }, {
+                data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+                label: "Europe",
+                borderColor: "#3cba9f",
+                fill: false
+            }, {
+                data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+                label: "Latin America",
+                borderColor: "#e8c3b9",
+                fill: false
+            }, {
+                data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+                label: "North America",
+                borderColor: "#c45850",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'World population per region (in millions)'
+            }
+        }
+    };
+    window.myLine = new Chart(ctx, config);
+}
+
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
+(function (factory) {
+    if (typeof define === "function" && define.amd) {
+
+        // AMD. Register as an anonymous module.
+        define(["../widgets/datepicker"], factory);
+    } else {
+
+        // Browser globals
+        factory(jQuery.datepicker);
+    }
+}(function (datepicker) {
+
+    datepicker.regional.ru = {
+        closeText: "Закрыть",
+        prevText: "&#x3C;Пред",
+        nextText: "След&#x3E;",
+        currentText: "Сегодня",
+        monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+        monthNamesShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн",
+            "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+        dayNames: ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"],
+        dayNamesShort: ["вск", "пнд", "втр", "срд", "чтв", "птн", "сбт"],
+        dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        weekHeader: "Нед",
+        dateFormat: "dd.mm.yy",
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
+    };
+    datepicker.setDefaults(datepicker.regional.ru);
+
+    return datepicker.regional.ru;
+
+}));
