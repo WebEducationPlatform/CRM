8c44ad15f478511fcc2eb320bc7aa42b7aceecd7
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-07-24 22:29:24 +0900

    Add local reload status list

--- src/main/resources/static/js/filtering-statuses.js
+++ src/main/resources/static/js/filtering-statuses.js
@@ -3,12 +3,14 @@
 function setFilterByMentor(mentorId,statusId) {
     $.post("/rest/client/filter", {newFilter: "BY_MENTOR", filterId: mentorId, statusId: statusId})
         .done(function () {
-            location.reload();
+            const url = "/status/" + statusId;
+            $("#clients-for-status" + statusId).load(url);
         });
 }
 function clearFilterByMentor(statusId) {
     $.post("/rest/client/filter/clear", { statusId: statusId})
         .done(function () {
-            location.reload();
+            const url = "/status/" + statusId;
+            $("#clients-for-status" + statusId).load(url);
         });
 }
