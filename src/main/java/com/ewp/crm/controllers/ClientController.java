4060bd8f9941678ce1f5aef9204a499dd0de1c4e
Sergey Kubatko <skubatko@gmail.com>
2019-07-27 15:02:40 +0300

    bugfix/dashboard/client-info/calls: добавил передачу недостающих сущностей в модель отображения карточек

--- src/main/java/com/ewp/crm/controllers/ClientController.java
+++ src/main/java/com/ewp/crm/controllers/ClientController.java
@@ -103,6 +103,7 @@ public class ClientController {
     public ModelAndView getAll(@AuthenticationPrincipal User userFromSession) {
 
         ModelAndView modelAndView = new ModelAndView("main-client-table");
+
         modelAndView.addObject("user", userFromSession);
 
         List<Role> sessionRoles = userFromSession.getRole();
@@ -120,30 +121,22 @@ public class ClientController {
             role = roleService.getRoleByName(ROLE_NAME_OWNER);
         }
         List<StatusDtoForBoard> statuses = statusService.getStatusesForBoardByUserAndRole(userFromSession, role);
-//        List<StatusDtoForBoard> statuses = StatusDtoForBoard.getListDtoStatuses(statusService.getAll());
         modelAndView.addObject("statuses", statuses);
 
         modelAndView.addObject("counter", new AtomicInteger(0));
+
+        List<User> userList = userService.getAll();
+        modelAndView.addObject("users", userList.stream().filter(User::isVerified).collect(Collectors.toList()));
+        modelAndView.addObject("newUsers", userList.stream().filter(x -> !x.isVerified()).collect(Collectors.toList()));
 
         List<Role> roles = roleService.getAll();
         roles.remove(roleService.getRoleByName("OWNER"));
         modelAndView.addObject("roles", roles);
 
-        List<User> userList = userService.getAll();
-        modelAndView.addObject("users", userList.stream().filter(User::isVerified).collect(Collectors.toList()));
-        modelAndView.addObject("users_without_mentors", userList.stream().filter(x -> !x.getRole().contains(roleService.getRoleByName("MENTOR"))).collect(Collectors.toList()));
-        modelAndView.addObject("newUsers", userList.stream().filter(x -> !x.isVerified()).collect(Collectors.toList()));
-        modelAndView.addObject("mentors", userList.stream().filter(x -> x.getRole().contains(roleService.getRoleByName("MENTOR"))).collect(Collectors.toList()));
-
         modelAndView.addObject("emailTmpl", messageTemplateService.getAll());
 
         modelAndView.addObject("slackWorkspaceUrl", slackService.getSlackWorkspaceUrl());
-
         modelAndView.addObject("notifications", notificationService.getByUserToNotify(userFromSession));
-        modelAndView.addObject("notifications_type_sms", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.SMS));
-        modelAndView.addObject("notifications_type_comment", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.COMMENT));
-        modelAndView.addObject("notifications_type_postpone", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.POSTPONE));
-        modelAndView.addObject("notifications_type_new_user", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.NEW_USER));
 
         return modelAndView;
     }
