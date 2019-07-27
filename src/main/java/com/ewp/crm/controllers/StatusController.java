02cbd49a19f1ee75a009e726d1686630e7e9e593
Sergey Kubatko <skubatko@gmail.com>
2019-07-27 16:28:38 +0300

    bugfix/dashboard/client-info/calls: добавил кэширование для отображение карточек на доске

--- src/main/java/com/ewp/crm/controllers/StatusController.java
+++ src/main/java/com/ewp/crm/controllers/StatusController.java
@@ -1,9 +1,6 @@
 package com.ewp.crm.controllers;
 
-import com.ewp.crm.models.Notification;
-import com.ewp.crm.models.Role;
-import com.ewp.crm.models.Status;
-import com.ewp.crm.models.User;
+import com.ewp.crm.models.*;
 import com.ewp.crm.models.dto.StatusDtoForBoard;
 import com.ewp.crm.service.interfaces.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +17,14 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.ewp.crm.util.Constants.*;
-import static com.ewp.crm.util.Constants.ROLE_NAME_ADMIN;
-import static com.ewp.crm.util.Constants.ROLE_NAME_OWNER;
 
 @Controller
 @PreAuthorize("hasAnyAuthority('ADMIN', 'USER', 'OWNER', 'HR', 'MENTOR')")
 @RequestMapping(value = "/status")
 public class StatusController {
+
+    private CachedStatusModelAttributes cachedStatusModelAttributes;
+
     private final StatusService statusService;
     private final MessageTemplateService messageTemplateService;
     private final NotificationService notificationService;
@@ -42,6 +40,7 @@ public class StatusController {
         this.notificationService = notificationService;
         this.userService = userService;
         this.roleService = roleService;
+        this.cachedStatusModelAttributes = new CachedStatusModelAttributes();
     }
 
     @GetMapping(value = "/{id}")
@@ -61,11 +60,29 @@ public class StatusController {
         if (statusOptional.isPresent()) {
             status = StatusDtoForBoard.getStatusDto(statusOptional.get());
         }
-
         model.addAttribute("status", status);
 
         model.addAttribute("user", userFromSession);
 
+        model.addAttribute("statuses", cachedStatusModelAttributes.statuses);
+        model.addAttribute("roles", cachedStatusModelAttributes.roles);
+
+        model.addAttribute("users", cachedStatusModelAttributes.users);
+        model.addAttribute("users_without_mentors", cachedStatusModelAttributes.usersWithoutMentors);
+        model.addAttribute("mentors", cachedStatusModelAttributes.mentors);
+
+        model.addAttribute("emailTmpl", cachedStatusModelAttributes.emailTmpl);
+
+        model.addAttribute("notifications", cachedStatusModelAttributes.notifications);
+        model.addAttribute("notifications_type_sms", cachedStatusModelAttributes.notificationsTypeSms);
+        model.addAttribute("notifications_type_comment", cachedStatusModelAttributes.notificationsTypeComment);
+        model.addAttribute("notifications_type_postpone", cachedStatusModelAttributes.notificationsTypePostpone);
+        model.addAttribute("notifications_type_new_user", cachedStatusModelAttributes.notificationsTypeNewUser);
+
+        return "fragments/htmlFragments::clientsForStatus";
+    }
+
+    public void prepareCachedStatusModelAttributes(User userFromSession) {
         List<Role> sessionRoles = userFromSession.getRole();
         Role role = roleService.getRoleByName(ROLE_NAME_USER);
         if (sessionRoles.contains(roleService.getRoleByName(ROLE_NAME_MENTOR))) {
@@ -80,27 +97,44 @@ public class StatusController {
         if (sessionRoles.contains(roleService.getRoleByName(ROLE_NAME_OWNER))) {
             role = roleService.getRoleByName(ROLE_NAME_OWNER);
         }
-        List<StatusDtoForBoard> statuses = statusService.getStatusesForBoardByUserAndRole(userFromSession, role);
-        model.addAttribute("statuses", statuses);
+        cachedStatusModelAttributes.statuses = statusService.getStatusesForBoardByUserAndRole(userFromSession, role);
 
         List<Role> roles = roleService.getAll();
         roles.remove(roleService.getRoleByName("OWNER"));
-        model.addAttribute("roles", roles);
+        cachedStatusModelAttributes.roles = roles;
 
         List<User> userList = userService.getAll();
-        model.addAttribute("users", userList.stream().filter(User::isVerified).collect(Collectors.toList()));
-        model.addAttribute("users_without_mentors", userList.stream().filter(x -> !x.getRole().contains(roleService.getRoleByName("MENTOR"))).collect(Collectors.toList()));
-        model.addAttribute("mentors", userList.stream().filter(x -> x.getRole().contains(roleService.getRoleByName("MENTOR"))).collect(Collectors.toList()));
+        cachedStatusModelAttributes.users = userList.stream().filter(User::isVerified).collect(Collectors.toList());
+        cachedStatusModelAttributes.usersWithoutMentors = userList.stream()
+                .filter(x -> !x.getRole().contains(roleService.getRoleByName("MENTOR")))
+                .collect(Collectors.toList());
+        cachedStatusModelAttributes.mentors = userList.stream()
+                .filter(x -> x.getRole().contains(roleService.getRoleByName("MENTOR")))
+                .collect(Collectors.toList());
+        cachedStatusModelAttributes.emailTmpl = messageTemplateService.getAll();
+        cachedStatusModelAttributes.notifications = notificationService.getByUserToNotify(userFromSession);
+        cachedStatusModelAttributes.notificationsTypeSms = notificationService
+                .getByUserToNotifyAndType(userFromSession, Notification.Type.SMS);
+        cachedStatusModelAttributes.notificationsTypeComment = notificationService
+                .getByUserToNotifyAndType(userFromSession, Notification.Type.COMMENT);
+        cachedStatusModelAttributes.notificationsTypePostpone = notificationService
+                .getByUserToNotifyAndType(userFromSession, Notification.Type.POSTPONE);
+        cachedStatusModelAttributes.notificationsTypeNewUser = notificationService
+                .getByUserToNotifyAndType(userFromSession, Notification.Type.NEW_USER);
+    }
 
-        model.addAttribute("emailTmpl", messageTemplateService.getAll());
-
-        model.addAttribute("notifications", notificationService.getByUserToNotify(userFromSession));
-        model.addAttribute("notifications_type_sms", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.SMS));
-        model.addAttribute("notifications_type_comment", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.COMMENT));
-        model.addAttribute("notifications_type_postpone", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.POSTPONE));
-        model.addAttribute("notifications_type_new_user", notificationService.getByUserToNotifyAndType(userFromSession, Notification.Type.NEW_USER));
-
-        return "fragments/htmlFragments::clientsForStatus";
+    private final class CachedStatusModelAttributes {
+        List<StatusDtoForBoard> statuses;
+        List<Role> roles;
+        List<User> users;
+        List<User> usersWithoutMentors;
+        List<User> mentors;
+        List<MessageTemplate> emailTmpl;
+        List<Notification> notifications;
+        List<Notification> notificationsTypeSms;
+        List<Notification> notificationsTypeComment;
+        List<Notification> notificationsTypePostpone;
+        List<Notification> notificationsTypeNewUser;
     }
 
 }
