0baec7bb832ea2412d389d39932002c2028bcced
proboostbiznes@gmail.com <gfhjkmlkzubnf1>
2019-07-28 20:54:43 +0300

    logs

--- src/main/java/com/ewp/crm/configs/inteface/SlackConfig.java
+++ src/main/java/com/ewp/crm/configs/inteface/SlackConfig.java
@@ -3,7 +3,7 @@ package com.ewp.crm.configs.inteface;
 public interface SlackConfig {
     String getAppFirstToken();
 
-    String getLegacyToke();
+    String getLegacyToken();
 
     String getGeneralChannelId();
 }
