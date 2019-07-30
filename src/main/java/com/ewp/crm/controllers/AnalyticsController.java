2da14d2e972f37da2eeb1527013175511754bad3
Sergey Kubatko <skubatko@gmail.com>
2019-07-30 13:54:11 +0300

    feature/analytics: исправил замечания после review

--- src/main/java/com/ewp/crm/controllers/AnalyticsController.java
+++ src/main/java/com/ewp/crm/controllers/AnalyticsController.java
@@ -1,7 +1,5 @@
 package com.ewp.crm.controllers;
 
-import com.ewp.crm.service.interfaces.StudentStatusService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 @PreAuthorize("hasAnyAuthority('OWNER','ADMIN')")
 public class AnalyticsController {
 
-    private final StudentStatusService studentStatus;
-
-    @Autowired
-    public AnalyticsController(StudentStatusService studentStatus) {
-        this.studentStatus = studentStatus;
-    }
-
     @GetMapping
-    public ModelAndView showAllManagers() {
+    public ModelAndView showAnalytics() {
         final ModelAndView modelAndView = new ModelAndView("analytics-table");
-
-        modelAndView.addObject("studentStatusList", studentStatus.getAll());
 
         return modelAndView;
     }
