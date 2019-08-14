59a0b437e71d0fa5a475bddba47d5f5801be0c71
Egor Kuznetsov <yaegorko@gmail.com>
2019-08-14 17:54:41 +0400

    fix lost students on  HR page

--- src/main/java/com/ewp/crm/controllers/HrController.java
+++ src/main/java/com/ewp/crm/controllers/HrController.java
@@ -1,25 +1,34 @@
 package com.ewp.crm.controllers;
 
+import com.ewp.crm.models.Client;
 import com.ewp.crm.models.SocialProfile;
+import com.ewp.crm.models.Status;
+import com.ewp.crm.models.dto.ClientDto;
+import com.ewp.crm.models.dto.ClientDtoForBoard;
 import com.ewp.crm.models.dto.HrDtoForBoard;
-import com.ewp.crm.service.interfaces.ClientService;
-import com.ewp.crm.service.interfaces.MessageTemplateService;
-import com.ewp.crm.service.interfaces.ProjectPropertiesService;
-import com.ewp.crm.service.interfaces.RoleService;
-import com.ewp.crm.service.interfaces.StatusService;
-import com.ewp.crm.service.interfaces.StudentStatusService;
-import com.ewp.crm.service.interfaces.UserService;
+import com.ewp.crm.service.interfaces.*;
+import com.google.gson.Gson;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import sun.net.www.http.HttpClient;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
@@ -28,61 +37,81 @@ import java.util.stream.Collectors;
 @PropertySource("file:./slackbot.properties")
 public class HrController {
 
-    @Value("${slackbot.domain}")
-    private String slackBotDomain;
+	@Value("${slackbot.domain}")
+	private String slackBotDomain;
+	@Value("${slackbot.access.protocol}")
+	private String slackBotAccessProtocol;
 
+	private final StatusService statusService;
+	private final ClientService clientService;
+	private final UserService userService;
+	private final MessageTemplateService messageTemplateService;
+	private final ProjectPropertiesService propertiesService;
+	private final StudentStatusService studentStatus;
+	private final RoleService roleService;
 
-    private final StatusService statusService;
-    private final ClientService clientService;
-    private final UserService userService;
-    private final MessageTemplateService messageTemplateService;
-    private final ProjectPropertiesService propertiesService;
-    private final StudentStatusService studentStatus;
-    private final RoleService roleService;
+	@Autowired
+	public HrController(StatusService statusService,
+	                    ClientService clientService,
+	                    UserService userService,
+	                    MessageTemplateService messageTemplateService,
+	                    ProjectPropertiesService propertiesService,
+	                    StudentStatusService studentStatus,
+	                    RoleService roleService) {
+		this.statusService = statusService;
+		this.clientService = clientService;
+		this.userService = userService;
+		this.messageTemplateService = messageTemplateService;
+		this.propertiesService = propertiesService;
+		this.studentStatus = studentStatus;
+		this.roleService = roleService;
+	}
 
-    @Autowired
-    public HrController(StatusService statusService,
-                        ClientService clientService,
-                        UserService userService,
-                        MessageTemplateService messageTemplateService,
-                        ProjectPropertiesService propertiesService,
-                        StudentStatusService studentStatus,
-                        RoleService roleService) {
-        this.statusService = statusService;
-        this.clientService = clientService;
-        this.userService = userService;
-        this.messageTemplateService = messageTemplateService;
-        this.propertiesService = propertiesService;
-        this.studentStatus = studentStatus;
-        this.roleService = roleService;
-    }
+	@GetMapping("/students")
+	public ModelAndView showAllStudents() {
+		HashMap<String, List<ClientDto>> lostStudentsInStatus = new HashMap<>();
+		List<Status> statuses = statusService.getAllStatusesForStudents();
+		statuses.forEach(status -> {
+			StringBuilder url = new StringBuilder(slackBotAccessProtocol + slackBotDomain + "/student/get/is_lost?emails=");
+			status.getClients().forEach(client -> {
+				if (client.getEmail().isPresent()) {
+					String email = client.getEmail().get();
+					url.append(email).append(",");
+				}
+			});
+			ResponseEntity<String[]> lostStudentsEmails = new RestTemplate().getForEntity(url.toString(), String[].class);
+			List<String> lostStudentsEmailsList = Arrays.asList(Objects.requireNonNull(lostStudentsEmails.getBody()));
+			List<ClientDto.ClientTransformer> clientList = clientService.getClientsDtoByEmails(lostStudentsEmailsList);
+			List<ClientDto> clients = new ArrayList<>();
+			clientList.forEach(clientTransformer ->
+					clients.add(new ClientDto(
+							clientTransformer.getClient_id(), clientTransformer.getFirst_name(), clientTransformer.getLast_name(),
+							"", clientTransformer.getClient_email())));
+			//lostStudentsEmailsList.forEach(email -> clientList.add(emailClientHashMap.get(email)));
+			lostStudentsInStatus.put(status.getName(), clients);
+		});
+		ModelAndView modelAndView = new ModelAndView("main-client-table-hr");
+		SocialProfile socialProfile = new SocialProfile();
+		modelAndView.addObject("slackBotDomain", slackBotDomain);
+		modelAndView.addObject("slackBotAccessProtocol", slackBotAccessProtocol);
+		modelAndView.addObject("statuses", lostStudentsInStatus);
+		modelAndView.addObject("projectProperties", propertiesService.get());
+		modelAndView.addObject("users", userService.getAll());
+		modelAndView.addObject("socialNetworkTypes", socialProfile.getAllSocialNetworkTypes());
+		modelAndView.addObject("emailTmpl", messageTemplateService.getAll());
+		modelAndView.addObject("studentStatuses", studentStatus.getAll());
+		return modelAndView;
+	}
 
-    @GetMapping("/students")
-    public ModelAndView showAllStudents() {
-        ModelAndView modelAndView = new ModelAndView("main-client-table-hr");
-        SocialProfile socialProfile = new SocialProfile();
-        modelAndView.addObject("allClients",
-                clientService.getAllClientsByPage(
-                        PageRequest.of(0, 15, Sort.by(Sort.Direction.DESC, "dateOfRegistration"))));
-        modelAndView.addObject("slackBotDomain", slackBotDomain);
-        modelAndView.addObject("statuses", statusService.getAll());
-        modelAndView.addObject("projectProperties", propertiesService.get());
-        modelAndView.addObject("users", userService.getAll());
-        modelAndView.addObject("socialNetworkTypes", socialProfile.getAllSocialNetworkTypes());
-        modelAndView.addObject("emailTmpl", messageTemplateService.getAll());
-        modelAndView.addObject("studentStatuses", studentStatus.getAll());
-        return modelAndView;
-    }
+	@GetMapping("/managers")
+	public ModelAndView showAllManagers() {
+		final ModelAndView modelAndView = new ModelAndView("hr-table");
 
-    @GetMapping("/managers")
-    public ModelAndView showAllManagers() {
-        final ModelAndView modelAndView = new ModelAndView("hr-table");
+		modelAndView.addObject("hrManagers",
+				userService.getByRole(roleService.getRoleByName("HR"))
+						.stream().map(HrDtoForBoard::new)
+						.collect(Collectors.toList()));
 
-        modelAndView.addObject("hrManagers",
-                userService.getByRole(roleService.getRoleByName("HR"))
-                        .stream().map(HrDtoForBoard::new)
-                        .collect(Collectors.toList()));
-
-        return modelAndView;
-    }
+		return modelAndView;
+	}
 }
