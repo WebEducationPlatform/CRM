8d8ae0095fc966f4aee9ce9188f8e6c3fc255b5d
nomokonov <vladimir.nomokonov@yandex.ru>
2019-09-02 17:38:17 +0900

    add CRUD operation for AutoAnswer by front

--- src/main/java/com/ewp/crm/controllers/rest/AutoAnswersRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/AutoAnswersRestController.java
@@ -1,18 +1,23 @@
 package com.ewp.crm.controllers.rest;
 
 import com.ewp.crm.models.AutoAnswer;
+import com.ewp.crm.models.MessageTemplate;
+import com.ewp.crm.models.Status;
 import com.ewp.crm.models.User;
+import com.ewp.crm.models.dto.AutoAnswerDto;
 import com.ewp.crm.service.interfaces.AutoAnswersService;
+import com.ewp.crm.service.interfaces.MessageTemplateService;
+import com.ewp.crm.service.interfaces.StatusService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest/autoanswers")
@@ -21,14 +26,26 @@ public class AutoAnswersRestController {
 
     private static Logger logger = LoggerFactory.getLogger(AutoAnswersRestController.class);
     private final AutoAnswersService autoAnswersService;
+    private final MessageTemplateService messageTemplateService;
+    private final StatusService statusService;
 
     @Autowired
-    public AutoAnswersRestController(AutoAnswersService autoAnswersService) {
+    public AutoAnswersRestController(AutoAnswersService autoAnswersService, MessageTemplateService messageTemplateService, StatusService statusService) {
         this.autoAnswersService = autoAnswersService;
+        this.messageTemplateService = messageTemplateService;
+        this.statusService = statusService;
+    }
+
+    @GetMapping(value = "/get/{id}")
+    public ResponseEntity getAutoAnswerById(@PathVariable("id") Long id){
+
+        AutoAnswer autoAnswer = autoAnswersService.get(id);
+        return  ResponseEntity.ok(new AutoAnswerDto(autoAnswer));
     }
 
     @PostMapping(value = "/add")
     public HttpStatus addNewAutoAnswer(
+            @RequestParam(name = "id") Long autoanswer_id,
             @RequestParam(name = "subject") String subject,
             @RequestParam(name = "messageTemplate") Long messageTemplate_id,
             @RequestParam(name = "status") Long status_id,
@@ -51,5 +68,31 @@ public class AutoAnswersRestController {
         return status;
     }
 
+    @PostMapping(value = "/update")
+    public HttpStatus updateNewAutoAnswer(
+            @RequestParam(name = "id") Long autoanswer_id,
+            @RequestParam(name = "subject") String subject,
+            @RequestParam(name = "messageTemplate") Long messageTemplate_id,
+            @RequestParam(name = "status") Long status_id,
+            @AuthenticationPrincipal User currentAdmin) {
+
+        HttpStatus httpStatus = HttpStatus.OK;
+        AutoAnswer autoAnswerFromDB = autoAnswersService.get(autoanswer_id);
+
+        MessageTemplate messageTemplate = messageTemplateService.get(messageTemplate_id);
+        Optional<Status> status = statusService.get(status_id);
+        if (autoAnswerFromDB!= null && messageTemplate != null && status.isPresent()){
+            autoAnswerFromDB.setSubject(subject);
+            autoAnswerFromDB.setMessageTemplate(messageTemplate);
+            autoAnswerFromDB.setStatus(status.get());
+
+            autoAnswersService.update(autoAnswerFromDB);
+            logger.info("{} has updated autoanswer theme: {}", currentAdmin.getFullName(), autoAnswerFromDB.getSubject());
+        }else {
+            httpStatus = HttpStatus.NOT_FOUND;
+        }
+
+        return httpStatus;
+    }
 
 }
