f8740db48ab66e6839ed81e55d228b5a2ae4c107
Sidorenkomv <50841585+Sidorenkomv@users.noreply.github.com>
2019-09-03 16:58:46 +0300

    Добавленные файлы для работы с сущностью UserStatus

--- src/main/java/com/ewp/crm/service/impl/UserStatusServiceImpl.java
+++ src/main/java/com/ewp/crm/service/impl/UserStatusServiceImpl.java
@@ -1,4 +1,66 @@
 package com.ewp.crm.service.impl;
 
-public class UserStatusServiceImpl {
+import com.ewp.crm.models.Role;
+import com.ewp.crm.models.Status;
+import com.ewp.crm.models.User;
+import com.ewp.crm.models.UserStatus;
+import com.ewp.crm.models.dto.StatusDtoForBoard;
+import com.ewp.crm.models.dto.StatusPositionIdNameDTO;
+import com.ewp.crm.repository.interfaces.UserStatusDAO;
+import com.ewp.crm.service.interfaces.UserStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserStatusServiceImpl implements UserStatusService {
+
+    @Autowired
+    UserStatusDAO userStatusDAO;
+
+    @Override
+    public void addStatusAllUsers(Status status) {
+        userStatusDAO.addStatusAllUsers(status);
+    }
+
+    @Override
+    public void addUserAllStatus(User user) {
+        userStatusDAO.addUserAllStatus(user);
+    }
+
+    @Override
+    public void deleteStatus(Long status_id) {
+        userStatusDAO.deleteStatus(status_id);
+    }
+
+    @Override
+    public void deleteUser(Long user_id) {
+        userStatusDAO.deleteUser(user_id);
+    }
+
+    @Override
+    public void updateUserStatus(Status status, User user) {
+        userStatusDAO.updateUserStatus(status, user);
+    }
+
+    @Override
+    public UserStatus getStatus(Long status_id, Long user_id) {
+        return userStatusDAO.getStatus(status_id, user_id);
+    }
+
+    @Override
+    public List<StatusPositionIdNameDTO> getAllStatusVisibleTrue(Long user_id) {
+        return userStatusDAO.getAllStatusVisibleTrue(user_id);
+    }
+
+    @Override
+    public List<StatusDtoForBoard> getStatusesForBoard(Long user_id, List<Role> roleList) {
+        return userStatusDAO.getStatusesForBoard(user_id, roleList);
+    }
+
+    @Override
+    public List<UserStatus> getAllUserStatus() {
+        return userStatusDAO.getAllUserStatus();
+    }
 }
--- src/main/java/com/ewp/crm/service/interfaces/UserStatusService.java
+++ src/main/java/com/ewp/crm/service/interfaces/UserStatusService.java
@@ -1,4 +1,31 @@
 package com.ewp.crm.service.interfaces;
 
+import com.ewp.crm.models.Role;
+import com.ewp.crm.models.Status;
+import com.ewp.crm.models.User;
+import com.ewp.crm.models.UserStatus;
+import com.ewp.crm.models.dto.StatusDtoForBoard;
+import com.ewp.crm.models.dto.StatusPositionIdNameDTO;
+
+import java.util.List;
+
 public interface UserStatusService {
+
+    void addStatusAllUsers(Status status);
+
+    void addUserAllStatus(User user);
+
+    void deleteStatus(Long status_id);
+
+    void deleteUser(Long user_id);
+
+    void updateUserStatus(Status status, User user);
+
+    UserStatus getStatus(Long status_id, Long user_id);
+
+    List<StatusPositionIdNameDTO> getAllStatusVisibleTrue(Long user_id);
+
+    List<StatusDtoForBoard> getStatusesForBoard(Long user_id, List<Role> roleList);
+
+    List<UserStatus> getAllUserStatus();
 }
