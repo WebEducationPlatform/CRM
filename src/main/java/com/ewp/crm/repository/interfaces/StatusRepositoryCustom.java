dc2843b08ce17cf993f6a392c5c1ae07f6d96a06
Sidorenkomv <50841585+Sidorenkomv@users.noreply.github.com>
2019-09-05 15:17:10 +0300

    3

--- src/main/java/com/ewp/crm/repository/interfaces/StatusRepositoryCustom.java
+++ src/main/java/com/ewp/crm/repository/interfaces/StatusRepositoryCustom.java
@@ -1,6 +1,9 @@
 package com.ewp.crm.repository.interfaces;
 
 import com.ewp.crm.models.Role;
+import com.ewp.crm.models.SortedStatuses;
+import com.ewp.crm.models.Status;
+import com.ewp.crm.models.User;
 import com.ewp.crm.models.dto.StatusDtoForBoard;
 import com.ewp.crm.models.dto.StatusDto;
 
@@ -12,4 +15,8 @@ public interface StatusRepositoryCustom {
     void transferClientsBetweenStatuses(Long statusFrom, Long statusTo);
 
     List<StatusDto> getStatusesForMailing();
+
+    SortedStatuses getSordedStatusBuId(Status status, User user);
+
+    void updateSortStatuses(Status status, User user, boolean isInvisible, Long position);
 }
