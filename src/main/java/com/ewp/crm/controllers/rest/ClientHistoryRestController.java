515d5292e57d75de3d9552b050bf0ce7024a7797
Kozionov <kozionov@rambler.ru>
2019-09-10 14:40:40 +0500

    Возвращение к исходному виду

--- src/main/java/com/ewp/crm/controllers/rest/ClientHistoryRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/ClientHistoryRestController.java
@@ -12,28 +12,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rest/client/history")
+@RequestMapping("/client/history/rest")
 public class ClientHistoryRestController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ClientHistoryRestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(ClientHistoryRestController.class);
 
-	private final ClientHistoryService clientHistoryService;
+    private final ClientHistoryService clientHistoryService;
 
-	@Value("10")
-	private int pageSize;
+    @Value("${project.pagination.page-size.client-history}")
+    private int pageSize;
 
-	@Autowired
-	public ClientHistoryRestController(ClientHistoryService clientHistoryService) {
-		this.clientHistoryService = clientHistoryService;
-	}
+    @Autowired
+    public ClientHistoryRestController(ClientHistoryService clientHistoryService) {
+        this.clientHistoryService = clientHistoryService;
+    }
 
-	@Autowired
-	CallRecordService callRecordService;
-
-	@GetMapping("/{clientId}")
-	public ResponseEntity getClientHistory(@PathVariable("clientId") long id, @RequestParam("page")int page, @RequestParam("isAsc")boolean isAsc) {
-		List<ClientHistoryDto> clientHistory = clientHistoryService.getAllDtoByClientId(id, page, pageSize, isAsc);
-		return ResponseEntity.ok(clientHistory);
-	}
+    @GetMapping("/getHistory/{clientId}")
+    public ResponseEntity getClientHistory(@PathVariable("clientId") long id, @RequestParam("page")int page, @RequestParam("isAsc")boolean isAsc) {
+        List<ClientHistoryDto> clientHistory = clientHistoryService.getAllDtoByClientId(id, page, pageSize, isAsc);
+        return ResponseEntity.ok(clientHistory);
+    }
 
 }
