715a982c44bc02778d13b95d1b4e256933abc2ad
Kozionov <kozionov@rambler.ru>
2019-09-10 14:38:00 +0500

    Возвращение к исходному виду

--- src/main/java/com/ewp/crm/controllers/rest/ClientHistoryRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/ClientHistoryRestController.java
@@ -1,20 +1,15 @@
 package com.ewp.crm.controllers.rest;
 
-import com.ewp.crm.models.CallRecord;
-import com.ewp.crm.models.ClientHistory;
 import com.ewp.crm.models.dto.ClientHistoryDto;
-import com.ewp.crm.repository.interfaces.ClientRepository;
 import com.ewp.crm.service.interfaces.CallRecordService;
 import com.ewp.crm.service.interfaces.ClientHistoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest/client/history")
@@ -33,9 +28,6 @@ public class ClientHistoryRestController {
 	}
 
 	@Autowired
-	private ClientRepository clientRepository;
-
-	@Autowired
 	CallRecordService callRecordService;
 
 	@GetMapping("/{clientId}")
@@ -44,32 +36,4 @@ public class ClientHistoryRestController {
 		return ResponseEntity.ok(clientHistory);
 	}
 
-	//Добавление записи по id клиента
-	@PostMapping("/{clientId}")
-	public ResponseEntity addClientHistory(@PathVariable("clientId") long id, @RequestBody ClientHistory clientHistory) {
-		clientHistory.setClient(clientRepository.getClientById(id));
-		Optional<ClientHistory> result = clientHistoryService.addHistory(clientHistory);
-		return ResponseEntity.ok(result);
-	}
-
-	//Удаление записи по id истории
-	@DeleteMapping("/{clientHistoryId}")
-	public ResponseEntity deleteClientHistory(@PathVariable("clientHistoryId") long clientHistoryId) {
-		//Проверяем есть ли связанная с ClientHistory запись callRecord и если есть удаляем ее.
-		Optional<CallRecord> callRecord = callRecordService.getByClientHistory_Id(clientHistoryId);
-		if (callRecord.isPresent()) {
-			callRecordService.delete(callRecord.get());
-		}
-		//Удаляем запись ClientHistory по Id
-		clientHistoryService.deleteClientHistoryById(clientHistoryId);
-		return new ResponseEntity(HttpStatus.valueOf(204));
-	}
-
-	//Обновление записи по id клиента
-	@PutMapping("/{clientId}")
-	public ResponseEntity updateClientHistory(@PathVariable("clientId") long id, @RequestBody ClientHistory clientHistory) {
-		clientHistory.setClient(clientRepository.getClientById(id));
-		Optional<ClientHistory> result = clientHistoryService.addHistory(clientHistory);
-		return ResponseEntity.ok(result);
-	}
 }
