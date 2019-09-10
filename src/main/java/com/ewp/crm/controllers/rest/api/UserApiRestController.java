1d4e2ccf596bd6fcfb58f266a833dcbe418c2e09
Sidorenkomv <50841585+Sidorenkomv@users.noreply.github.com>
2019-09-10 10:35:15 +0300

    Создание rest.api контроллера для User с тестами

--- src/main/java/com/ewp/crm/controllers/rest/api/UserApiRestController.java
+++ src/main/java/com/ewp/crm/controllers/rest/api/UserApiRestController.java
@@ -1,39 +1,56 @@
 package com.ewp.crm.controllers.rest.api;
 
 import com.ewp.crm.models.User;
-import com.ewp.crm.service.interfaces.UserService;
+import com.ewp.crm.service.interfaces.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/rest/api/user/")
-@Transactional
 public class UserApiRestController {
 
-    @Autowired
-    UserService userService;
+    private static Logger logger = LoggerFactory.getLogger(UserApiRestController.class);
+
+    private final UserService userService;
+    private final UserStatusService userStatusService;
+    private final ObjectMapper mapper;
+    private final ClientService clientService;
+    private final CommentService commentService;
+    private final SMSInfoService smsInfoService;
 
     @Autowired
-    UserStatusService userService;
-
-    @Autowired
-    private ObjectMapper mapper;
+    public UserApiRestController(UserService userService,
+                                 UserStatusService userStatusService,
+                                 ObjectMapper mapper,
+                                 ClientService clientService,
+                                 CommentService commentService,
+                                 SMSInfoService smsInfoService) {
+        this.userService = userService;
+        this.userStatusService = userStatusService;
+        this.mapper = mapper;
+        this.clientService = clientService;
+        this.commentService = commentService;
+        this.smsInfoService = smsInfoService;
+    }
 
     @GetMapping("/")
     public List<User> getAllUsers() {
         List<User> userList = userService.getAll();
+        logger.info("All users are received");
         return userList;
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable("id") Long id) {
         User user = userService.get(id);
+        logger.info("The user is returned: " + id);
         return user;
     }
 
@@ -41,8 +58,12 @@ public class UserApiRestController {
     public ResponseEntity addUser(@RequestBody User user) {
         try {
             userService.add(user);
+            User userResult = userService.getUserByEmail(user.getEmail()).get();
+            userStatusService.addUserForAllStatuses(userResult.getId());
+            logger.info("The user has been added: " + user.getFullName());
             return ResponseEntity.ok(HttpStatus.OK);
         }catch (Exception e) {
+            logger.error("An error occurred while adding a user: " + e.getMessage());
             return ResponseEntity.ok(HttpStatus.NOT_FOUND);
         }
     }
@@ -51,9 +72,30 @@ public class UserApiRestController {
     public ResponseEntity updateUser(@RequestBody User user) {
         try {
             userService.update(user);
+            logger.info("User changes were successful: " + user.getFullName());
             return ResponseEntity.ok(HttpStatus.OK);
         }catch (Exception e) {
+            logger.error("An error occurred while changing the user: " + e.getMessage());
             return ResponseEntity.ok(HttpStatus.NOT_MODIFIED);
+        }
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity deleteUserTransferClients(@RequestParam("id_delete") Long id,
+                                                    @RequestParam("id_transfer") Long id_transfer) {
+        try {
+            User deletedUser = userService.get(id);
+            User receiver = userService.get(id_transfer);
+            clientService.transferClientsBetweenOwners(deletedUser, receiver);
+            commentService.deleteAllCommentsByUserId(id);
+            smsInfoService.deleteAllSMSByUserId(id);
+            userService.delete(id);
+            userStatusService.deleteUser(id);
+            logger.info("User deletion was successful: " + deletedUser.getFullName());
+            return ResponseEntity.ok(HttpStatus.OK);
+        }catch (Exception e) {
+            logger.error("An error occurred while deleting a user: " + e.getMessage());
+            return ResponseEntity.ok(HttpStatus.NOT_FOUND);
         }
     }
 
@@ -61,8 +103,11 @@ public class UserApiRestController {
     public ResponseEntity deleteUser(@PathVariable("id") Long id) {
         try {
             userService.delete(id);
+            userStatusService.deleteUser(id);
+            logger.info("User deletion was successful: " + id);
             return ResponseEntity.ok(HttpStatus.OK);
         }catch (Exception e) {
+            logger.error("An error occurred while deleting a user: " + e.getMessage());
             return ResponseEntity.ok(HttpStatus.NOT_FOUND);
         }
     }
--- src/test/java/com/ewp/crm/rest/api/UserApiRestControllerTest.java
+++ src/test/java/com/ewp/crm/rest/api/UserApiRestControllerTest.java
@@ -1,46 +1,35 @@
 package com.ewp.crm.rest.api;
 
-import com.ewp.crm.models.Client;
-import com.ewp.crm.models.User;
-import com.ewp.crm.service.interfaces.RoleService;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.*;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.client.RestTemplate;
-
-import javax.xml.bind.DatatypeConverter;
-import java.time.LocalDate;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class UserApiRestControllerTest {
 
-    private String url = "http://localhost:9999/rest/api/user/";
+    @LocalServerPort
+    private int port;
+
+    private String url = "http://localhost:" + port + "/rest/api/user/";
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private RoleService roleService;
 
     @Autowired
     private ObjectMapper mapper;
@@ -65,26 +54,6 @@ public class UserApiRestControllerTest {
 
     @Test
     public void addUser() throws Exception {
-//        Не использую сериализацию, потому-что пароль закрыт на сериализацию
-//        User user = new User(
-//                "Maksim",
-//                "Sidorenko",
-//                LocalDate.of(1982, 03, 02),
-//                "89885648715",
-//                "sir.SidorenkoMV@yandex.ru",
-//                "admin",
-//                null,
-//                Client.Sex.MALE.toString(),
-//                "Taganrog",
-//                "Russia",
-//                Arrays.asList(roleService.getRoleByName("USER"), roleService.getRoleByName("ADMIN"),
-//                        roleService.getRoleByName("OWNER")),
-//                true,
-//                true);
-//
-//        mapper.configure(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS, true);
-//        String json = mapper.writeValueAsString(user);
-
         String json = "{\"id\":null," +
                 "\"firstName\":\"Maksim\"," +
                 "\"lastName\":\"Sidorenko\"," +
@@ -124,19 +93,83 @@ public class UserApiRestControllerTest {
         assertTrue(result.getResponse().getContentAsString().equals("\"OK\""));
     }
 
-    //    @Test
-//    public void updateUser() throws Exception {
-//        mockMvc.perform(get(url + "9"))
-//                .andDo(print())
-//                .andExpect(content().contentType("application/json;charset=UTF-8"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//    }
-//    @Test
-//    public void deleteUser() throws Exception {
-//        mockMvc.perform(delete(url + "23"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andReturn();
-//    }
+        @Test
+    public void updateUser() throws Exception {
+
+            String json = "{\"id\":31," +
+                    "\"firstName\":\"Maksim\"," +
+                    "\"lastName\":\"Sidorenko\"," +
+                    "\"birthDate\":\"1982-03-02\"," +
+                    "\"phoneNumber\":\"89885648716\"," + //изменил телефон
+                    "\"email\":\"sir.SidorenkoMV@yandex.ru\"," +
+                    "\"password\":\"admin\"," +
+                    "\"vk\":null," +
+                    "\"sex\":\"MALE\"," +
+                    "\"city\":\"Taganrog\"," +
+                    "\"country\":\"Russia\"," +
+                    "\"photo\":null," +
+                    "\"photoType\":null," +
+                    "\"ipTelephony\":true," +
+                    "\"newClientNotifyIsEnabled\":true," +
+                    "\"autoAnswer\":null," +
+                    "\"vkToken\":null," +
+                    "\"googleToken\":null," +
+                    "\"enableMailNotifications\":false," +
+                    "\"enableSmsNotifications\":false," +
+                    "\"enableAsignMentorMailNotifications\":false," +
+                    "\"role\":[{\"id\":2,\"roleName\":\"USER\"},{\"id\":1,\"roleName\":\"ADMIN\"},{\"id\":3,\"roleName\":\"OWNER\"}]," +
+                    "\"colorBackground\":null," +
+                    "\"studentPageFilters\":null," +
+                    "\"lastClientDate\":null," +
+                    "\"verified\":true," +
+                    "\"enabled\":true," +
+                    "\"rowStatusDirection\":false}";
+
+            MvcResult result = mockMvc.perform(put(url)
+                    .contentType(APPLICATION_JSON_VALUE)
+                    .content(json)
+                    .accept(APPLICATION_JSON_VALUE))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andReturn();
+            assertTrue(result.getResponse().getContentAsString().equals("\"OK\""));
+    }
+
+    @Test
+    public void deleteUserTransferClients() throws Exception {
+        MvcResult result = mockMvc.perform(delete(url + "?id_delete=33&id_transfer=21"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().equals("\"OK\""));
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        MvcResult result = mockMvc.perform(delete(url + "32"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().equals("\"OK\""));
+    }
 }
+
+//        Не использую сериализацию, потому-что пароль закрыт на сериализацию
+//        User user = new User(
+//                "Maksim",
+//                "Sidorenko",
+//                LocalDate.of(1982, 03, 02),
+//                "89885648715",
+//                "sir.SidorenkoMV@yandex.ru",
+//                "admin",
+//                null,
+//                Client.Sex.MALE.toString(),
+//                "Taganrog",
+//                "Russia",
+//                Arrays.asList(roleService.getRoleByName("USER"), roleService.getRoleByName("ADMIN"),
+//                        roleService.getRoleByName("OWNER")),
+//                true,
+//                true);
+//
+//        mapper.configure(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS, true);
+//        String json = mapper.writeValueAsString(user);
