4706bfa96555bb0af13ed3eb5699cca3377622a7
skrasnogolovy <skrasnogolovy@yandex.ru>
2019-09-15 19:36:52 +0300

    Stanislav Krasnogolovy Created CommentApiRestControllerTest

---
+++ src/test/java/com/ewp/crm/controllers/rest/api/CommentApiRestControllerTest.java
@@ -0,0 +1,16 @@
+package com.ewp.crm.controllers.rest.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentApiRestControllerTest {
+
+    @Test
+    public void addComments(){
+
+    }
+}
