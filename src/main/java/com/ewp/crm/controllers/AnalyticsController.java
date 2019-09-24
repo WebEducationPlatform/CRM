adbacb7cf69dbb246de9278a3178bd4a826d9a59
Kozionov <kozionov@rambler.ru>
2019-09-24 16:35:30 +0500

    нужно ввыпилить из проекта ВСЁ, что связано с аналитикой вкладка “аналитика” и протестить что ничего не сломалось скорее всего придется переписывваать какие-то связи и тп поэтому обязателльно приложи скл запросы для изменения базы

--- src/main/java/com/ewp/crm/controllers/AnalyticsController.java
+++ src/main/java/com/ewp/crm/controllers/AnalyticsController.java
@@ -1,6 +1,5 @@
 package com.ewp.crm.controllers;
 
-import com.ewp.crm.models.dto.StatusDto;
 import com.ewp.crm.service.interfaces.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -8,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/analytics")
@@ -25,9 +22,7 @@ public class AnalyticsController {
 
     @GetMapping
     public ModelAndView showAnalytics() {
-        List<StatusDto> studentStatuses = statusService.getAllStatusesIdsForStudents();
         final ModelAndView modelAndView = new ModelAndView("analytics-table");
-        modelAndView.addObject("studentStatuses", studentStatuses);
         return modelAndView;
     }
 
