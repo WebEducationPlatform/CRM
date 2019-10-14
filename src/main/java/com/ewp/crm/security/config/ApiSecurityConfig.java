4a8c4f9bff481fdb41cf007e59f0bd2824d8b401
Kozionov <kozionov@rambler.ru>
2019-10-14 10:14:36 +0500

    ApiSecurityConfig

--- src/main/java/com/ewp/crm/security/config/ApiSecurityConfig.java
+++ src/main/java/com/ewp/crm/security/config/ApiSecurityConfig.java
@@ -4,9 +4,10 @@ import com.ewp.crm.security.jwt.JwtTokenFilter;
 import com.ewp.crm.security.jwt.JwtTokenProvider;
 import com.ewp.crm.security.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,12 +21,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
-    private final AuthenticationService authenticationService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Autowired
-    public ApiSecurityConfig(JwtTokenProvider jwtTokenProvider, AuthenticationService authenticationService) {
+    public ApiSecurityConfig(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
-        this.authenticationService = authenticationService;
     }
 
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,8 +50,4 @@ public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-        @Autowired
-        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(authenticationService);
-        }
 }
